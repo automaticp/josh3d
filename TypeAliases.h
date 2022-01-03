572d9fb62c5df44ddc851d030241433107ad4586
automaticp <automaticpte@gmail.com>
2022-01-03 19:46:02 +0300

    refactored the project structure; split source files into libs

--- TypeAliases.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <array>
-#include <functional>
-#include <glad/glad.h>
-
-template <typename T>
-using refw = std::reference_wrapper<T>;
---
+++ src/types/TypeAliases.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <functional>
+
+template <typename T>
+using refw = std::reference_wrapper<T>;
