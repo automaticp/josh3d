95fb896173763b1ec72e6d8bc288056b457f4dc2
automaticp <automaticpte@gmail.com>
2022-01-06 21:29:11 +0300

    added/removed comments

--- src/resource/glResource.h
+++ src/resource/glResource.h
@@ -3,16 +3,11 @@
 #include "TypeAliases.h"
 // TODO: implement some actual ownership of the Id through smart pointers with custom deallocators 
 // maybe have like a templated glResource<std::unique_ptr<GLuint, glDeallocator<Derived>>> with CRTP in Derived classes
-// TODO: maybe also allow for dynamic memory managment though "std::shared_ptr<bool> m_isDestroyed" and conditional destructor;
-// the above idea sound like some dangerous garbo, tbh
 
+// Base class for OpenGL resources that carry a handle (Shaders, Textures, VBOs, etc.). RAII-enabled.
 class glResource {
 protected:
-	GLuint m_id{ 0 };
-
-	// all resources have to be acquired in the constructors of the Derived classes;
-	//     Example defenition: glGenBuffers(1, &m_id);
-
+	GLuint m_id{ 0 }; // null-defaulted as deleting a null handle has no effect in OpenGL
 
 public:
 	GLuint getId() const noexcept { return m_id; }
@@ -24,7 +19,10 @@ public:
 	template <typename T> requires std::convertible_to<T, GLuint>
 	operator T() const = delete;
 
+	// all resources have to be acquired in the constructors of the Derived classes;
+	//     Example defenition body: glGenBuffers(1, &m_id);
 	glResource() = default;
+
 	// disallow copying, allow moving
 	glResource(const glResource& other) = delete;
 	glResource& operator=(const glResource& other) = delete;
@@ -32,7 +30,8 @@ public:
 	glResource& operator=(glResource&& other) = default;
 
 	// wraps the call to OpenGL to release a resource;
-	//     Example defenition: glDeleteBuffers(1, &m_id);
+	// all resources have to be released in the virtual destructors of the Derived classes;
+	//     Example defenition body: glDeleteBuffers(1, &m_id);
 	virtual ~glResource() = 0;
 
 };
