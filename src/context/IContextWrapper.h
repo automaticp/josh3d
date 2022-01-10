c1947a777b060a9b8e88b1ae2185af92c0d900d4
automaticp <automaticpte@gmail.com>
2022-01-11 02:54:34 +0300

    implemented abstract IContextWrapper base

--- src/context/IContextWrapper.h
+++ src/context/IContextWrapper.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <GLFW/glfw3.h>
+
+
+class IContextWrapper {
+public:
+	// provides abstract interface for RAII-enabled context creation
+	// also implicitly disables copy and move operations
+	virtual ~IContextWrapper() = 0;
+};
+
+inline IContextWrapper::~IContextWrapper() = default;
