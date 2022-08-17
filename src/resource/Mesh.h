aa607897d51abbf8bf2e4e38b3012d99e23d16c5
automaticp <automaticpte@gmail.com>
2022-08-17 23:34:10 +0300

    added a bunch of code to mesh and moderl, reimplemented textures

--- src/resource/Mesh.h
+++ src/resource/Mesh.h
@@ -15,7 +15,9 @@
 #include <iostream>
 #include <span>
 #include <cassert>
-
+#include <cstddef>
+#include <variant>
+#include <stdexcept>
 
 struct AttributeParams {
     GLuint      index;
@@ -120,6 +122,11 @@ public:
         );
     }
 
+
+    void unbind() {
+        glBindVertexArray(0u);
+    }
+
 };
 
 
@@ -152,6 +159,10 @@ public:
         vao.associate_with(*this, aparams);
         return *this;
     }
+
+    void unbind() {
+        glBindBuffer(GL_ARRAY_BUFFER, 0u);
+    }
 };
 
 
@@ -177,6 +188,10 @@ public:
         );
         return *this;
     }
+
+    void unbind() {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0u);
+    }
 };
 
 
@@ -189,6 +204,133 @@ vbo.bind().attach_data(...).associate_with(bvao, ...);
 ebo.bind(bvao).attach_data(...);
 */
 
+class TextureData;
+class BoundTextureHandle;
+
+class TextureHandle : public TextureAllocator {
+public:
+    BoundTextureHandle bind() {
+        glBindTexture(GL_TEXTURE_2D, id_);
+        return {};
+    }
+
+    BoundTextureHandle bind_to_unit(GLenum tex_unit) {
+        set_active_unit(tex_unit);
+        bind();
+        return {};
+    }
+
+    static void set_active_unit(GLenum tex_unit) { glActiveTexture(tex_unit); }
+
+};
+
+
+class BoundTextureHandle {
+public:
+
+    BoundTextureHandle& attach_data(const TextureData& tex_data,
+        GLenum internal_format = GL_RGBA, GLenum format = GL_NONE) {
+
+        if (format == GL_NONE) {
+            switch (tex_data.n_channels()) {
+                case 1ull: format = GL_RED; break;
+                case 2ull: format = GL_RG; break;
+                case 3ull: format = GL_RGB; break;
+                case 4ull: format = GL_RGBA; break;
+                default: format = GL_RED;
+            }
+        }
+
+        glTexImage2D(
+            GL_TEXTURE_2D, 0, static_cast<GLint>(internal_format),
+            tex_data.width(), tex_data.height(), 0,
+            format, GL_UNSIGNED_BYTE, tex_data.data()
+        );
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        return *this;
+    }
+
+};
+
+
+// Texture data variants
+
+class StbImageData {
+private:
+    using stb_deleter_t = decltype(
+        [](std::byte* data) {
+            if (data) { stbi_image_free(reinterpret_cast<stbi_uc*>(data)); }
+        }
+    );
+    using stb_image_owner_t = std::unique_ptr<std::byte[], stb_deleter_t>;
+
+    size_t width_;
+    size_t height_;
+    size_t n_channels_;
+    stb_image_owner_t data_{};
+
+public:
+    explicit StbImageData(const std::string& path, int num_desired_channels = 0) {
+
+        stbi_set_flip_vertically_on_load(true);
+
+        int width, height, n_channels;
+        data_.reset(
+            reinterpret_cast<std::byte*>(
+                stbi_load(path.c_str(), &width, &height, &n_channels, num_desired_channels)
+            )
+        );
+        if (!data_) {
+            throw std::runtime_error("Stb could not load the image at " + path);
+        }
+
+        this->width_      = width;
+        this->height_     = height;
+        this->n_channels_ = n_channels;
+    }
+
+    size_t size() const noexcept { return width_ * height_ * n_channels_; }
+    std::byte* data() const noexcept { return data_.get(); }
+    size_t width() const noexcept { return width_; }
+    size_t height() const noexcept { return height_; }
+    size_t n_channels() const noexcept { return n_channels_; }
+
+};
+
+
+class ImageData {
+private:
+    size_t width_;
+    size_t height_;
+    size_t n_channels_;
+    std::unique_ptr<std::byte[]> data_;
+
+public:
+    ImageData(size_t width, size_t height, size_t n_channels)
+        : width_{ width }, height_{ height }, n_channels_{ n_channels },
+        data_{ std::make_unique_for_overwrite<std::byte[]>(size()) } {}
+
+    size_t size() const noexcept { return width_ * height_ * n_channels_; }
+    std::byte* data() const noexcept { return data_.get(); }
+    size_t width() const noexcept { return width_; }
+    size_t height() const noexcept { return height_; }
+    size_t n_channels() const noexcept { return n_channels_; }
+
+};
+
+
+class TextureData : public std::variant<ImageData, StbImageData> {
+public:
+    size_t size() const noexcept { std::visit([](auto&& v) { return v.size(); }, *this); }
+    std::byte* data() const noexcept { std::visit([](auto&& v) { return v.data(); }, *this); }
+    size_t width() const noexcept { std::visit([](auto&& v) { return v.width(); }, *this); }
+    size_t height() const noexcept { std::visit([](auto&& v) { return v.height(); }, *this); }
+    size_t n_channels() const noexcept { std::visit([](auto&& v) { return v.n_channels(); }, *this); }
+};
+
+
+
 } // namespace detail
 
 
@@ -199,8 +341,9 @@ private:
     std::vector<V> vertices;
     std::vector<GLuint> elements;
 
-    Texture diffuse_texture;
-    Texture specular_texture;
+    // FIXME: separate texture handle from data
+    detail::TextureHandle diffuse_texture;
+    detail::TextureHandle specular_texture;
 
     detail::VBO vbo;
     detail::VAO vao;
@@ -208,11 +351,9 @@ private:
 
 public:
     Mesh(std::vector<V> vertices, std::vector<GLuint> elements,
-         const std::string& diffuse_filename,
-         const std::string& specular_filename)
+         detail::TextureHandle diffuse, detail::TextureHandle specular)
         : vertices{ std::move(vertices) }, elements{ std::move(elements) },
-        diffuse_texture{ diffuse_filename },
-        specular_texture{ specular_filename }
+        diffuse_texture{ std::move(diffuse) }, specular_texture{ std::move(specular) }
     {
 
         auto bvao = vao.bind();
@@ -229,10 +370,10 @@ public:
     void draw(ShaderProgram& sp) {
 
         sp.setUniform("material.diffuse", 0);
-	    diffuse_texture.setActiveUnitAndBind(0);
+	    diffuse_texture.bind_to_unit(GL_TEXTURE0);
 
         sp.setUniform("material.specular", 1);
-	    specular_texture.setActiveUnitAndBind(1);
+	    specular_texture.bind_to_unit(GL_TEXTURE1);
 
         sp.setUniform("material.shininess", 128.0f);
 
@@ -266,13 +407,14 @@ public:
         }
     }
 
+private:
 
     Model& retrieve_mesh_data(Assimp::Importer& importer,
                               const std::string& path)
     {
         const aiScene* scene{
             importer.ReadFile(path,
-                aiProcess_Triangulate || aiProcess_FlipUVs ||
+                aiProcess_Triangulate | aiProcess_FlipUVs |
                 aiProcess_ImproveCacheLocality
             )
         };
@@ -283,13 +425,60 @@ public:
             // Myself. Out the window.
         }
 
-
+        meshes.reserve(scene->mNumMeshes);
+        process_node(scene->mRootNode, scene);
 
         return *this;
     }
 
-    Mesh get_mesh_data(const aiMesh* mesh) {
-        //return Mesh(get_vertex_data<V>(mesh), get_element_data(mesh), ...);
+    void process_node(aiNode* node, const aiScene* scene) {
+
+        for ( auto&& mesh_id : std::span(node->mMeshes, node->mNumMeshes) ) {
+            aiMesh* mesh{ scene->mMeshes[mesh_id] };
+            meshes.emplace_back(get_mesh_data(mesh, scene));
+        }
+
+        for ( auto&& child : std::span(node->mChildren, node->mNumChildren) ) {
+            process_node(child, scene);
+        }
+    }
+
+
+
+    Mesh<V> get_mesh_data(const aiMesh* mesh, const aiScene* scene) {
+        if (mesh->mMaterialIndex >= 0) {
+            aiMaterial* material{ scene->mMaterials[mesh->mMaterialIndex] };
+            detail::TextureHandle diffuse  = get_texture_from_material(material, aiTextureType_DIFFUSE);
+            detail::TextureHandle specular = get_texture_from_material(material, aiTextureType_SPECULAR);
+            return Mesh<V>(
+                get_vertex_data<V>(mesh), get_element_data(mesh), std::move(diffuse), std::move(specular)
+            );
+        } else {
+            throw std::runtime_error("The requested mesh has no valid material index");
+        }
+    }
+
+
+    TextureHandle get_texture_from_material(const aiMaterial* material, aiTextureType type) {
+
+        assert(material->GetTextureCount(type) == 1);
+
+        aiString path;
+        material->GetTexture(type, 0ull, &path);
+
+        detail::TextureData tex_data{ path.C_Str() };
+        detail::TextureHandle tex;
+
+        // FIXME: this is ewww
+        gl::GLenum tex_unit;
+        switch (type) {
+            case aiTextureType_SPECULAR: tex_unit = GL_TEXTURE1; break;
+            default:
+            case aiTextureType_DIFFUSE:  tex_unit = GL_TEXTURE0; break;
+        }
+
+        tex.bind_to_unit(tex_unit).attach_data(tex_data);
+        return tex;
     }
 
 
