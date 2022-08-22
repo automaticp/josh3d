0a1d780210c9ec888326c32d6e2243673639537c
automaticp <automaticpte@gmail.com>
2022-08-22 05:14:24 +0300

    added a simple ShaderSource class

---
+++ src/resource/ShaderSource.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <string>
+#include <variant>
+#include <utility>
+#include <fstream>
+
+
+namespace learn {
+
+
+
+struct FileReader {
+    std::string operator()(const std::string& path) {
+        std::ifstream file{ path };
+        return std::string{
+            std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()
+        };
+    }
+};
+
+
+class ShaderSource {
+private:
+    std::string text_;
+
+public:
+    template<typename Reader, typename... Args>
+    ShaderSource(Reader&& reader, Args&&... args) : text_{ reader(args...) } {}
+
+    ShaderSource(std::string text) : text_{ std::move(text) } {}
+
+    operator const std::string& () const noexcept {
+        return text_;
+    }
+
+    const std::string& text() const noexcept {
+        return text_;
+    }
+};
+
+
+
+} // namespace learn
