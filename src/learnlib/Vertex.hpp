dcdccccfde33e767a1fdd453d2b35de94bf9cb0b
automaticp <automaticpte@gmail.com>
2022-08-23 02:26:26 +0300

    moved VertexTraits and AttributeParams into a separate header

--- src/learnlib/Vertex.hpp
+++ src/learnlib/Vertex.hpp
@@ -1,37 +1,22 @@
 #pragma once
 #include <array>
+#include <vector>
+#include <span>
 #include <cstddef>
 #include <glbinding/gl/gl.h>
 #include <glm/glm.hpp>
 
+#include "VertexTraits.hpp"
 #include "AssimpModelLoader.hpp"
-
 
 namespace learn {
 
-
-
-struct AttributeParams {
-    gl::GLuint      index;
-    gl::GLint       size;
-    gl::GLenum      type;
-    gl::GLboolean   normalized;
-    gl::GLsizei     stride_bytes;
-    gl::GLint64     offset_bytes;
-};
 
 
 struct Vertex {
     glm::vec3 position;
     glm::vec3 normal;
     glm::vec2 tex_uv;
-};
-
-
-template<typename V>
-struct VertexTraits {
-    static_assert(sizeof(V) == 0, "Custom vertex class V must have a VertexTraits<V> specialization. There's no default that makes sense.");
-    constexpr static std::array<AttributeParams, 0> aparams;
 };
 
 
