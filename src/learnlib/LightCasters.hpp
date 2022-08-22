4b29f30de7737fb971b33ad843b0a16a82f02c1d
automaticp <automaticpte@gmail.com>
2022-08-23 01:55:31 +0300

    namespaced LightCasters

---
+++ src/learnlib/LightCasters.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <glm/glm.hpp>
+
+
+namespace learn {
+
+
+namespace light {
+
+
+struct Attenuation {
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+
+struct Directional {
+    glm::vec3 color;
+    glm::vec3 direction;
+};
+
+struct Point {
+    glm::vec3 color;
+    glm::vec3 position;
+    Attenuation attenuation;
+};
+
+struct Spotlight {
+    glm::vec3 color;
+    glm::vec3 position;
+    glm::vec3 direction;
+    Attenuation attenuation;
+    float innerCutoffRad;
+    float outerCutoffRad;
+};
+
+
+} // namespace light
+
+
+} // namespace learn
