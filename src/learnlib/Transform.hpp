cf4c4119602547edb1cfd1e9bc8f8734ca272ded
automaticp <automaticpte@gmail.com>
2022-12-30 18:15:37 +0600

    add rvalue versions of transformations

--- src/learnlib/Transform.hpp
+++ src/learnlib/Transform.hpp
@@ -22,17 +22,35 @@ public:
         return glm::mat3(glm::transpose(glm::inverse(model_)));
     }
 
-    Transform& translate(const glm::vec3& delta) noexcept {
+
+    Transform& translate(const glm::vec3& delta) & noexcept {
         model_ = glm::translate(model_, delta);
         return *this;
     }
 
-    Transform& rotate(float angle_rad, const glm::vec3& axis) noexcept {
+    Transform translate(const glm::vec3& delta) && noexcept {
+        model_ = glm::translate(model_, delta);
+        return *this;
+    }
+
+
+    Transform& rotate(float angle_rad, const glm::vec3& axis) & noexcept {
         model_ = glm::rotate(model_, angle_rad, axis);
         return *this;
     }
 
-    Transform& scale(const glm::vec3& xyz_scaling) noexcept {
+    Transform rotate(float angle_rad, const glm::vec3& axis) && noexcept {
+        model_ = glm::rotate(model_, angle_rad, axis);
+        return *this;
+    }
+
+
+    Transform& scale(const glm::vec3& xyz_scaling) & noexcept {
+        model_ = glm::scale(model_, xyz_scaling);
+        return *this;
+    }
+
+    Transform scale(const glm::vec3& xyz_scaling) && noexcept {
         model_ = glm::scale(model_, xyz_scaling);
         return *this;
     }
