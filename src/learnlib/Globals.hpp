3b489958111ece0e30e1a9a9af11ed178099ac03
automaticp <automaticpte@gmail.com>
2022-12-30 19:54:45 +0600

    add support for global pools

---
+++ src/learnlib/Globals.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "DataPool.hpp"
+#include "GLObjectPool.hpp"
+#include "TextureData.hpp"
+#include "GLObjects.hpp"
+
+namespace learn::globals {
+
+void clear_all();
+
+inline DataPool<TextureData> texture_data_pool;
+inline GLObjectPool<TextureHandle> texture_handle_pool{ texture_data_pool };
+
+
+// Clear out all the global pools.
+// Must be done before destroying the OpenGL context.
+inline void clear_all() {
+    texture_data_pool.clear();
+    texture_handle_pool.clear();
+}
+
+} // namespace learn::globals
