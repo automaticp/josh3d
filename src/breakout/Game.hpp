c6ef7786f9bf23177cddd70c8a2f68bca25ee3c3
automaticp <automaticpte@gmail.com>
2023-01-04 18:51:57 +0600

    move collision code into a separate file

--- src/breakout/Game.hpp
+++ src/breakout/Game.hpp
@@ -207,69 +207,11 @@ private:
 
     void update_ball_movement() {
 
-        // Super messy
         auto dxdy = ball_.velocity() * frame_timer_.delta<float>();
 
-        enum class RectXCollisionType {
-            none, left, right
-        };
-        enum class RectYCollisionType {
-            none, top, bottom
-        };
-
-        struct InnerRectCollisionInfo {
-            RectXCollisionType x_collision{ RectXCollisionType::none };
-            RectYCollisionType y_collision{ RectYCollisionType::none };
-            glm::vec2 overshoot{ 0.f, 0.f };
-
-            bool did_collide() const noexcept {
-                return x_collision != RectXCollisionType::none
-                    || y_collision != RectYCollisionType::none;
-            }
-        };
-
-        auto inner_ball_on_rect_collision =
-            [](const Rect2D& rect, const Ball& ball, glm::vec2 dxdy)
-                -> InnerRectCollisionInfo
-            {
-
-                const glm::vec2 new_pos = ball.center() + dxdy;
-                const glm::vec2 move_direction = glm::sign(dxdy);
-                const glm::vec2 new_edge_pos = new_pos + move_direction * ball.radius();
-
-                InnerRectCollisionInfo info{};
-                auto& [x_collision, y_collision, overshoot] = info;
-
-                if (new_edge_pos.x > rect.bound_right()) {
-
-                    overshoot.x = new_edge_pos.x - rect.bound_right();
-                    x_collision = RectXCollisionType::right;
-
-                } else if (new_edge_pos.x < rect.bound_left()) {
-
-                    overshoot.x = new_edge_pos.x - rect.bound_left();
-                    x_collision = RectXCollisionType::left;
-
-                }
-
-
-                if (new_edge_pos.y > rect.bound_top()) {
-
-                    overshoot.y = new_edge_pos.y - rect.bound_top();
-                    y_collision = RectYCollisionType::top;
-
-                } else if (new_edge_pos.y < rect.bound_bottom()) {
-
-                    overshoot.y = new_edge_pos.y - rect.bound_bottom();
-                    y_collision = RectYCollisionType::bottom;
-
-                }
-
-                return info;
-            };
-
         // Resolve the collision with the canvas edges
-        auto canvas_collision = inner_ball_on_rect_collision(global_canvas, ball_, dxdy);
+        auto canvas_collision =
+            inner_ball_on_rect_collision(global_canvas, ball_, dxdy);
         if (canvas_collision.did_collide()) {
             if (canvas_collision.x_collision != RectXCollisionType::none) {
                 ball_.velocity().x = -ball_.velocity().x;
@@ -278,121 +220,7 @@ private:
                 ball_.velocity().y = -ball_.velocity().y;
             }
             dxdy = dxdy - (2.f * canvas_collision.overshoot);
-
-            std::clog << "Canvas Overshoot:   (" << canvas_collision.overshoot.x << ", "
-                << canvas_collision.overshoot.y << ")\n";
         }
-
-
-        // The side of the rectangle
-        // that the ball collided with,
-        // or 'none' if no collision occured.
-        enum class RectCollisionType : size_t {
-            none=0, left, right, top, bottom
-        };
-
-
-        auto outer_collision_direction =
-            [](glm::vec2 difference_vector) -> RectCollisionType {
-                constexpr std::array<glm::vec2, 4> compass{ {
-                    { -1.f, 0.f }, // left
-                    { 1.f, 0.f },  // right
-                    { 0.f, 1.f },  // top
-                    { 0.f, -1.f }  // bottom
-                } };
-
-                auto me = ranges::max_element(compass, {},
-                    [&](const glm::vec2& dir) {
-                        return glm::dot(dir, -difference_vector);
-                    }
-                );
-                size_t elem_idx = ranges::distance(ranges::begin(compass), me);
-                return RectCollisionType{ elem_idx + 1 };
-            };
-
-        struct OuterRectCollisionInfo {
-            RectCollisionType type{ RectCollisionType::none };
-            glm::vec2 difference{ 0.f, 0.f };
-
-            bool did_collide() const noexcept {
-                return type != RectCollisionType::none;
-            }
-        };
-
-        auto outer_ball_on_rect_collision =
-            [&](const Rect2D& rect, const Ball& ball, glm::vec2 dxdy)
-                -> OuterRectCollisionInfo
-            {
-
-                const glm::vec2 new_ball_center =
-                    ball.center() + dxdy;
-
-                const glm::vec2 center_difference =
-                    new_ball_center - rect.center;
-
-                const glm::vec2 clamped =
-                    glm::clamp(
-                        center_difference,
-                        -rect.half_size(),
-                        rect.half_size()
-                    );
-
-                const glm::vec2 closest = rect.center + clamped;
-                const glm::vec2 difference = closest - new_ball_center;
-
-                bool overlaps =
-                    glm::length(difference) < ball.radius();
-
-                if (overlaps) {
-                    return {
-                        .type=outer_collision_direction(difference),
-                        .difference=difference
-                    };
-                } else {
-                    return {};
-                }
-            };
-
-
-
-        auto apply_outer_collision_correction =
-            [](Ball& ball, glm::vec2& dxdy, const OuterRectCollisionInfo& collision) {
-
-                const auto&[dir, diff_vector] = collision;
-
-                if (dir == RectCollisionType::left ||
-                    dir == RectCollisionType::right) {
-
-                        ball.velocity().x *= -1.f;
-
-                        const float penetration =
-                            ball.radius() - glm::abs(diff_vector.x);
-
-                        std::clog << "Tile X Penetration: " << penetration << '\n';
-
-                        if (dir == RectCollisionType::left) {
-                            dxdy.x -= 2.f * penetration;
-                        } else {
-                            dxdy.x += 2.f * penetration;
-                        }
-                    } else /* vertical */ {
-
-                        ball.velocity().y *= -1.f;
-
-                        const float penetration =
-                            ball.radius() - glm::abs(diff_vector.y);
-
-                        std::clog << "Tile Y Penetration: " << penetration << '\n';
-
-                        if (dir == RectCollisionType::top) {
-                            dxdy.y += 2.f * penetration;
-                        } else {
-                            dxdy.y -= 2.f * penetration;
-                        }
-                    }
-
-            };
-
 
 
         // Resolve collisions with tiles
@@ -412,6 +240,7 @@ private:
             }
         }
 
+        // Resolve the collision with the paddle
         if (!ball_.is_stuck()) {
 
             auto paddle_collision =
