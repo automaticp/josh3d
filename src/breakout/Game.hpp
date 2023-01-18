2279d21776a44ae18d7d8b1a0dad2fb7a6a6bf91
automaticp <automaticpte@gmail.com>
2023-01-18 13:22:41 +0600

    move FXState into a separate file

--- src/breakout/Game.hpp
+++ src/breakout/Game.hpp
@@ -19,7 +19,7 @@
 #include "SpriteRenderer.hpp"
 #include "PostprocessDoubleBuffer.hpp"
 #include "PostprocessRenderer.hpp"
-#include "glfwpp/window.h"
+#include "FXState.hpp"
 
 #include <array>
 #include <glbinding/gl/enum.h>
@@ -38,57 +38,6 @@ enum class GameState {
     active, win, menu
 };
 
-
-enum class FXType : size_t {
-    shake = 0,
-    speed = 1,
-    sticky = 2,
-    pass_through = 3,
-    pad_size_up = 4,
-    confuse = 5,
-    chaos = 6
-};
-
-
-class FXState {
-private:
-    struct FX {
-        float time{ 0.f };
-        bool enabled{ false };
-
-        void enable(float duration) noexcept {
-            enabled = true;
-            time = duration;
-        }
-
-        void update(float dt) noexcept {
-            time -= dt;
-            if (time <= 0.f) [[likely]] { enabled = false; }
-        }
-    };
-
-    std::array<FX, 7> effects_{};
-
-    FX& get_fx(FXType type) noexcept {
-        return effects_[static_cast<std::underlying_type_t<FXType>>(type)];
-    }
-
-public:
-    void update(float dt) noexcept {
-        for (auto& fx : effects_) {
-            fx.update(dt);
-        }
-    }
-
-    void enable(FXType type, float duration) {
-        get_fx(type).enable(duration);
-    }
-
-    bool is_active(FXType type) {
-        return get_fx(type).enabled;
-    }
-
-};
 
 
 class Game {
