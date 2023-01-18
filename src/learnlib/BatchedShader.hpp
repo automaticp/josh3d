a209e1a1bfaffed29d6762fba3bb021058fe1131
automaticp <automaticpte@gmail.com>
2023-01-18 23:52:59 +0600

    add a toy implementation for some details of BatchedShader

--- src/learnlib/BatchedShader.hpp
+++ src/learnlib/BatchedShader.hpp
@@ -1,10 +1,48 @@
 #pragma once
 #include "GLObjects.hpp"
+#include "Logging.hpp"
+#include <glbinding/gl/enum.h>
+#include <glbinding/gl/functions-patches.h>
+#include <glbinding/gl/functions.h>
 #include <glbinding/gl/gl.h>
+#include <glbinding/gl/types.h>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
 
 namespace learn {
 
+
+struct UniformInfo {
+    gl::GLint index;
+    gl::GLint size;
+    gl::GLenum type;
+};
+
+
 class UniformMap {
+private:
+    std::unordered_map<std::string, UniformInfo> info_;
+
+public:
+    void emplace(const std::string& name, UniformInfo info) {
+        info_.emplace(name, info);
+    }
+
+    void emplace(std::string_view name, UniformInfo info) {
+        info_.emplace(std::string(name), info);
+    }
+
+    gl::GLint location_of(const std::string& name) {
+        return info_.at(name).index;
+    }
+
+    const auto& map() const noexcept {
+        return info_;
+    }
+
     // TODO: implement later
     //
     // A wrapper around a map:
@@ -29,16 +67,63 @@ private:
     ShaderProgram shader_;
     UniformMap uniforms_;
 
+    void query_uniforms() {
+        using namespace gl;
+
+        GLint num_uniforms;
+        glGetProgramiv(shader_, GL_ACTIVE_UNIFORMS, &num_uniforms);
+
+        GLint max_name_size;
+        glGetProgramiv(shader_, GL_ACTIVE_UNIFORM_MAX_LENGTH, &max_name_size);
+
+        std::vector<GLchar> buffer(static_cast<size_t>(max_name_size));
+
+        for (GLint i{ 0 }; i < num_uniforms; ++i) {
+            GLint uniform_size;
+            GLenum uniform_type;
+            GLsizei name_size;
+            glGetActiveUniform(shader_, i, max_name_size, &name_size, &uniform_size, &uniform_type, buffer.data());
+            uniforms_.emplace(
+                std::string_view(buffer.begin(), buffer.begin() + name_size),
+                { i, uniform_size, uniform_type }
+            );
+        }
+
+
+    }
+
 public:
-    BatchedShader(/* shaders */)
-    // : shader_{
-    //     ShaderBuilder().add_...(shaders).get()
-    // }
+    BatchedShader(ShaderProgram&& shader)
+        : shader_{ std::move(shader) }
     {
         // TODO:
         // Query the names and types of the uniforms
         // and fill out the uniform map.
         // Use glGetActiveUniform() for querying.
+        query_uniforms();
+    }
+
+    ShaderProgram& program() noexcept { return shader_; }
+
+    gl::GLint location_of(const std::string& name) {
+        return uniforms_.location_of(name);
+    }
+
+    // This is mostly for testing.
+    // Make sure the current program is active.
+    void uniform(const std::string& name, auto... args) {
+        ActiveShaderProgram::uniform(uniforms_.location_of(name), args...);
+	}
+
+    void print_uniforms() const {
+        for (auto& info : uniforms_.map()) {
+            global_logstream
+                << "[Uniform] name: " << info.first
+                << ", index: " << info.second.index
+                << ", type: " << static_cast<std::underlying_type_t<decltype(info.second.type)>>(info.second.type)
+                << ", size: " << info.second.size
+                << "\n";
+        }
     }
 
     gl::GLenum type_of(/* string name */) {
