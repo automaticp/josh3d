106d1022032a35e1cad3370109937e6578942325
automaticp <automaticpte@gmail.com>
2023-01-27 00:35:16 +0600

    add a basic ImGui input blocker

--- src/learnlib/ImGuiContextWrapper.hpp
+++ src/learnlib/ImGuiContextWrapper.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
+
 namespace glfw { class Window; }
 
 namespace learn {
+
+
+class KeyCallbackArgs;
+class CursorPosCallbackArgs;
+class ScrollCallbackArgs;
+
+
+class ImGuiInputBlocker {
+public:
+    bool is_key_blocked(const KeyCallbackArgs&) const noexcept;
+    bool is_cursor_blocked(const CursorPosCallbackArgs&) const noexcept;
+    bool is_scroll_blocked(const ScrollCallbackArgs&) const noexcept;
+};
 
 
 class ImGuiContextWrapper {
