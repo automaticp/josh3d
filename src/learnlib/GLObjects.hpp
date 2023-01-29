c2a9550d996a6e769844cc2d28e710dfc618f467
automaticp <automaticpte@gmail.com>
2023-01-29 15:06:29 +0600

    add and_then_with_self chainer for self propagation

--- src/learnlib/GLObjects.hpp
+++ src/learnlib/GLObjects.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <concepts>
 #include <glbinding/gl/bitfield.h>
 #include <glbinding/gl/boolean.h>
 #include <glbinding/gl/enum.h>
@@ -57,7 +58,7 @@ A generic trait that allows you to invoke any
 callable during bound state. Shold help minimize
 creation of bound dummies as lvalues in certain cases.
 
-Example:
+Example 1:
 
 dst_framebuffer.bind_as(GL_DRAW_FRAMEBUFFER)
     .and_then([&] {
@@ -67,6 +68,23 @@ dst_framebuffer.bind_as(GL_DRAW_FRAMEBUFFER)
             .unbind();
     })
     .unbind();
+
+
+Example 2:
+
+vao_.bind()
+    .and_then_with_self([&](BoundVAO& self) {
+
+        vbo_.bind()
+            .attach_data(data.vertices().size(), data.vertices().data(), GL_STATIC_DRAW)
+            .associate_with(self, VertexTraits<V>::aparams);
+
+        ebo_.bind(self)
+            .attach_data(data.elements().size(), data.elements().data(), GL_STATIC_DRAW);
+
+    })
+    .unbind();
+
 */
 template<typename CRTP>
 class AndThen {
@@ -83,6 +101,26 @@ public:
         f();
         return static_cast<const CRTP&>(*this);
     }
+
+
+    template<std::invocable<CRTP&> CallableT>
+    CRTP& and_then_with_self(CallableT&& f)
+        noexcept(noexcept(f(static_cast<CRTP&>(*this))))
+    {
+        f(static_cast<CRTP&>(*this));
+        return static_cast<CRTP&>(*this);
+    }
+
+    template<std::invocable<const CRTP&> CallableT>
+    const CRTP& and_then_with_self(CallableT&& f) const
+        noexcept(noexcept(f(static_cast<const CRTP&>(*this))))
+    {
+        f(static_cast<const CRTP&>(*this));
+        return static_cast<const CRTP&>(*this);
+    }
+
+
+
 };
 
 } // namespace detail
