cadf5181e00145705b9501fd284ab18ac0b9d8ca
automaticp <automaticpte@gmail.com>
2023-01-31 23:59:57 +0600

    set wrap rules for depth render target

--- src/learnlib/RenderTargetDepth.hpp
+++ src/learnlib/RenderTargetDepth.hpp
@@ -3,7 +3,6 @@
 #include <glbinding/gl/gl.h>
 
 
-
 namespace learn {
 
 
@@ -23,12 +22,16 @@ public:
     {
         using namespace gl;
 
+        const float border_color[4] = { 1.f, 1.f, 1.f, 1.f };
+
         tex_.bind()
             .specify_image(width_, height_, GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr)
             .set_parameter(GL_TEXTURE_MIN_FILTER, GL_NEAREST)
             .set_parameter(GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-            .set_parameter(GL_TEXTURE_WRAP_S, GL_REPEAT)
-            .set_parameter(GL_TEXTURE_WRAP_T, GL_REPEAT);
+            .set_parameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
+            .set_parameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
+            // Maybe don't do this in the render target though?
+            .set_parameter(GL_TEXTURE_BORDER_COLOR, border_color);
 
         fbo_.bind()
             .attach_texture(tex_, GL_DEPTH_ATTACHMENT)
