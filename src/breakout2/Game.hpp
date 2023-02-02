72cbcee28d9c94b3ecb8b321b9409533cc33dcd0
automaticp <automaticpte@gmail.com>
2023-02-03 00:29:18 +0600

    move Transform2D into a separate header

--- src/breakout2/Game.hpp
+++ src/breakout2/Game.hpp
@@ -10,18 +10,6 @@
 #include <glm/glm.hpp>
 #include <queue>
 
-struct Transform2D {
-    glm::vec2 position;
-    glm::vec2 scale;
-    float angle_rad;
-
-    learn::MTransform mtransform() const noexcept {
-        return learn::Transform()
-            .scale({ scale, 1.f })
-            .rotate(angle_rad, { 0.f, 0.f, 1.f })
-            .translate({ position, 0.f });
-    }
-};
 
 struct Velocity2D {
     glm::vec2 velocity;
