7b50e0cd41bb06be535c27a29749f09cca50e547
automaticp <automaticpte@gmail.com>
2023-02-05 13:56:10 +0600

    move Rect2D into Canvas since its not used anywhere else

--- src/breakout2/Canvas.hpp
+++ src/breakout2/Canvas.hpp
@@ -1,4 +1,52 @@
 #pragma once
-#include "Rect2D.hpp"
+#include <glm/glm.hpp>
+#include <glm/vector_relational.hpp>
+#include <glm/ext.hpp>
 
+
+// Helper class for working with square bounds.
+struct Rect2D {
+    glm::vec2 center;
+    glm::vec2 size;
+
+    constexpr Rect2D(glm::vec2 center, glm::vec2 size_xy)
+        : center{ center }, size{ size_xy } {}
+
+    // { x0, y0, x1, y1 }
+    constexpr Rect2D(glm::vec4 verts)
+        : center{
+            midpoint({ verts[0], verts[2] }),
+            midpoint({ verts[1], verts[3] })
+        },
+        size{
+            glm::abs(verts[0] - verts[2]),
+            glm::abs(verts[1] - verts[3]),
+        }
+    {}
+
+    constexpr float bound_left()   const noexcept { return center.x - size.x / 2.0f; }
+    constexpr float bound_right()  const noexcept { return center.x + size.x / 2.0f; }
+    constexpr float bound_bottom() const noexcept { return center.y - size.y / 2.0f; }
+    constexpr float bound_top()    const noexcept { return center.y + size.y / 2.0f; }
+
+    constexpr float width()  const noexcept { return glm::abs(size.x); }
+    constexpr float height() const noexcept { return glm::abs(size.y); }
+
+    constexpr glm::vec2 half_size() const noexcept { return size / 2.0f; }
+
+    constexpr bool contains(glm::vec2 point) const noexcept {
+        glm::vec2 dxdy = glm::abs(point - center);
+        return glm::all(glm::lessThanEqual(dxdy, half_size()));
+    }
+
+private:
+    constexpr static float midpoint(glm::vec2 segment) noexcept {
+        return 0.5f * (segment[0] + segment[1]);
+    }
+
+};
+
+
+
+// Defines the global screen-space canvas.
 inline constexpr Rect2D canvas{ glm::vec4{ 0.f, 0.f, 1600.f, 900.f } };
