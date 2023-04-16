2b6719de5e986a0f27f90d4b83b1b3b4c0c066dd
automaticp <automaticpte@gmail.com>
2023-04-16 14:52:39 +0600

    add type introspection support and other common helpers to UniqueFunction

--- src/learnlib/util/UniqueFunction.hpp
+++ src/learnlib/util/UniqueFunction.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include <concepts>
+#include <cstddef>
 #include <memory>
+#include <cassert>
 #include <functional>
 #include <type_traits>
+#include <typeinfo>
 #include <utility>
 
 
@@ -24,18 +27,24 @@ private:
     class UFBase {
     public:
         virtual ResT operator()(ArgTs...) = 0;
+        virtual const std::type_info& type() const noexcept = 0;
         virtual ~UFBase() = default;
     };
 
     template<typename CallableT>
     class UFConcrete : public UFBase {
-    private:
-        CallableT target_;
     public:
-        UFConcrete(CallableT&& target) : target_{ std::move(target) } {}
+        CallableT target;
+
+        UFConcrete(CallableT&& target) : target{ std::move(target) } {}
         ResT operator()(ArgTs... args) override {
-            return std::invoke(target_, std::forward<ArgTs>(args)...);
+            return std::invoke(target, std::forward<ArgTs>(args)...);
         }
+
+        const std::type_info& type() const noexcept override {
+            return typeid(CallableT);
+        }
+
     };
 
     std::unique_ptr<UFBase> target_ptr_;
@@ -57,7 +66,29 @@ public:
     }
 
     ResT operator()(ArgTs... args) {
+        assert(target_ptr_ && "UniqueFunction with no target has been invoked");
         return std::invoke(*target_ptr_, std::forward<ArgTs>(args)...);
+    }
+
+    template<typename CallableT>
+    CallableT* target() noexcept {
+        return try_get_target<CallableT>();
+    }
+
+    template<typename CallableT>
+    const CallableT* target() const noexcept {
+        return try_get_target<CallableT>();
+    }
+
+    const std::type_info& target_type() const noexcept {
+        if (!target_ptr_) { return typeid(void); }
+        return typeid(target_ptr_->type());
+    }
+
+    operator bool() const noexcept { return bool(target_ptr_); }
+
+    void swap(UniqueFunction& other) noexcept {
+        std::swap(target_ptr_, other.target_ptr_);
     }
 
     UniqueFunction(const UniqueFunction&) = delete;
@@ -65,10 +96,32 @@ public:
 
     UniqueFunction(UniqueFunction&& other) noexcept = default;
     UniqueFunction& operator=(UniqueFunction&& other) noexcept = default;
+
+private:
+    // Does not propagate constness to the target.
+    // Use public target() functions for that.
+    template<typename CallableT>
+    CallableT* try_get_target() const noexcept {
+        auto* wrapper_ptr = dynamic_cast<UFConcrete<CallableT>*>(target_ptr_.get());
+        return wrapper_ptr ? &wrapper_ptr->target : nullptr;
+    }
 };
 
 
+// ADL pls
+template<typename ResT, typename ...ArgTs>
+void swap(UniqueFunction<ResT(ArgTs...)>& lhs,
+    UniqueFunction<ResT(ArgTs...)>& rhs) noexcept
+{
+    lhs.swap(rhs);
+}
 
+template<typename ResT, typename ...ArgTs>
+bool operator==(const UniqueFunction<ResT(ArgTs...)>& fun,
+    std::nullptr_t) noexcept
+{
+    return !fun;
+}
 
 
 
