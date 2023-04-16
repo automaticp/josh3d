2a9d355290e9f221e9c9f041caa1d4b4e9ff6a5d
automaticp <automaticpte@gmail.com>
2023-04-16 17:35:21 +0600

    add ULocation - a wrapper for a uniform location

---
+++ src/learnlib/gl/ULocation.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <glbinding/gl/gl.h>
+
+
+namespace learn {
+
+// A simple transparent wrapper around a uniform location value.
+// Mostly used because it's default c-tor sets location to -1.
+struct ULocation {
+    gl::GLint value{ -1 };
+
+    ULocation() = default;
+    ULocation(gl::GLint value) : value{ value } {}
+
+    constexpr operator gl::GLint() const noexcept {
+        return value;
+    }
+};
+
+} // namespace learn
