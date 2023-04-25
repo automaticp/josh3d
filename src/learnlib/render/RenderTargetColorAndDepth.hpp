16d4a45067212088e7a63ec5b3c78ce876c51798
automaticp <automaticpte@gmail.com>
2023-04-25 22:16:09 +0600

    add a render target for rendering both color and depth to textures

---
+++ src/learnlib/render/RenderTargetColorAndDepth.hpp
@@ -0,0 +1,91 @@
+#pragma once
+#include "GLObjects.hpp"
+#include <glbinding/gl/enum.h>
+#include <glbinding/gl/gl.h>
+
+
+
+namespace learn {
+
+
+class RenderTargetColorAndDepth {
+private:
+    TextureHandle color_;
+    TextureHandle depth_;
+    Framebuffer fbo_;
+
+    gl::GLsizei width_;
+    gl::GLsizei height_;
+
+    gl::GLenum color_format_;
+    gl::GLenum color_type_;
+
+public:
+    RenderTargetColorAndDepth(
+        gl::GLsizei width, gl::GLsizei height,
+        gl::GLenum color_format = gl::GL_RGBA,
+        gl::GLenum color_type = gl::GL_UNSIGNED_BYTE
+    )
+        : width_{ width }
+        , height_{ height }
+        , color_format_{ color_format }
+        , color_type_{ color_type }
+    {
+        using namespace gl;
+
+        color_.bind()
+            .specify_image(width_, height_, color_format_,
+                color_format_, color_type_, nullptr)
+            .set_parameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+            .set_parameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            .set_parameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
+            .set_parameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+        depth_.bind()
+            .specify_image(width_, height_, GL_DEPTH_COMPONENT,
+                GL_DEPTH_COMPONENT, GL_FLOAT, nullptr)
+            .set_parameter(GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+            .set_parameter(GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            .set_parameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
+            .set_parameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+        fbo_.bind()
+            .attach_texture(color_, GL_COLOR_ATTACHMENT0)
+            .attach_texture(depth_, GL_DEPTH_ATTACHMENT)
+            .unbind();
+
+    }
+
+    TextureHandle& color_target() noexcept { return color_; }
+    const TextureHandle& color_target() const noexcept { return color_; }
+
+    TextureHandle& depth_target() noexcept { return depth_; }
+    const TextureHandle& depth_target() const noexcept { return depth_; }
+
+    Framebuffer& framebuffer() noexcept { return fbo_; }
+
+    gl::GLsizei width() const noexcept { return width_; }
+    gl::GLsizei height() const noexcept { return height_; }
+
+
+    void reset_size(gl::GLsizei width, gl::GLsizei height) {
+        using namespace gl;
+
+        width_ = width;
+        height_ = height;
+
+        color_.bind()
+            .specify_image(width_, height_, color_format_,
+                color_format_, color_type_, nullptr);
+
+        depth_.bind()
+            .specify_image(width_, height_, GL_DEPTH_COMPONENT,
+                GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+
+    }
+
+
+};
+
+
+} // namespace learn
