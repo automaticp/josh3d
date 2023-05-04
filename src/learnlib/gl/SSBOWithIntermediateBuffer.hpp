0957292c5788fd0d3fd70986aacbcff9b437bb79
automaticp <automaticpte@gmail.com>
2023-05-04 10:45:31 +0600

    update the interface of SSBO wrapper: add ability to bind directly, give read access to storage, fix updating from sentineled ranges

--- src/learnlib/gl/SSBOWithIntermediateBuffer.hpp
+++ src/learnlib/gl/SSBOWithIntermediateBuffer.hpp
@@ -58,20 +58,51 @@ public:
 
         const bool was_resized = old_size != new_size;
 
-        ssbo_.bind_to(binding).and_then_with_self([&, this](BoundSSBO& ssbo) {
-            if (was_resized) {
-                ssbo.attach_data(storage_.size(), storage_.data(), usage_);
-            } else [[likely]] {
-                ssbo.sub_data(storage_.size(), 0, storage_.data());
-            }
-        });
+        update_ssbo_from_ready_storage(was_resized);
+
         return was_resized;
     }
 
     bool update(std::ranges::input_range auto&& range) {
-        return update(std::begin(range), std::end(range));
+        const size_t old_size = storage_.size();
+
+        // This alternative implementation would not exist
+        // if the std::distance or std::vector::insert would
+        // accept iterator-sentinel pairs...
+
+        storage_.clear();
+        storage_.reserve(std::ranges::distance(range));
+        for (T&& value : range) {
+            storage_.emplace_back(value);
+        }
+
+        const size_t new_size = storage_.size();
+
+        const bool was_resized = old_size != new_size;
+
+        update_ssbo_from_ready_storage(was_resized);
+
+        return was_resized;
     }
 
+    const std::vector<T>& storage() const noexcept { return storage_; }
+
+    BoundSSBO bind() {
+        return ssbo_.bind_to(binding);
+    }
+
+private:
+    void update_ssbo_from_ready_storage(bool needs_resizing) {
+        ssbo_.bind_to(binding)
+            .and_then_with_self([&, this](BoundSSBO& ssbo) {
+                if (needs_resizing) {
+                    ssbo.attach_data(storage_.size(), storage_.data(), usage_);
+                } else [[likely]] {
+                    ssbo.sub_data(storage_.size(), 0, storage_.data());
+                }
+            }
+        );
+    }
 };
 
 
