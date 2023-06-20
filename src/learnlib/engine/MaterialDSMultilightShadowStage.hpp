1bed94bcc4d305a914eb6ad1a6831f2e4f22aa90
automaticp <automaticpte@gmail.com>
2023-06-20 06:17:42 +0600

    minor style fixes for forward rendering stage: move memfun defs outside, add a comment about the class

--- src/learnlib/engine/MaterialDSMultilightShadowStage.hpp
+++ src/learnlib/engine/MaterialDSMultilightShadowStage.hpp
@@ -27,7 +27,15 @@
 
 namespace learn {
 
+/*
+Forward rendering stage for meshes with diffuse-specular material.
 
+Supports ambient light, 1 directional light with (optional) shadows,
+and "unbounded" number of point lights with (optional) shadows.
+
+You'll run out of frames and memory for large number
+of point light shadows (>2) though.
+*/
 class MaterialDSMultilightShadowStage {
 private:
     ShaderProgram sp_{
@@ -152,7 +160,7 @@ public:
     float     dir_light_cam_offset{ 100.f };
     gl::GLint dir_light_pcf_samples{ 1 };
 
-public:
+
     MaterialDSMultilightShadowStage() = default;
 
     void operator()(const RenderEngine::PrimaryInterface& engine, const entt::registry& registry) {
@@ -169,232 +177,21 @@ public:
     }
 
 private:
-    void prepare_point_lights(const RenderEngine::PrimaryInterface&, const entt::registry& registry) {
-
-        using namespace gl;
-
-        // Update SSBOs for point lights.
-
-        auto plights_with_shadow_view = registry.view<const light::Point, const components::ShadowCasting>();
-
-        const size_t old_size = plights_with_shadows_ssbo_.size();
-
-        plights_with_shadows_ssbo_.bind().update(
-            plights_with_shadow_view | ranges::views::transform([&](entt::entity e) {
-                return plights_with_shadow_view.get<const light::Point>(e);
-            })
-        );
-
-        const bool num_plights_with_shadows_changed =
-            old_size != plights_with_shadows_ssbo_.size();
-
-        if (num_plights_with_shadows_changed) {
-            plight_shadow_maps.reset_size(
-                plight_shadow_maps.width(), plight_shadow_maps.height(),
-                static_cast<GLint>(plights_with_shadows_ssbo_.size())
-            );
-        }
-
-
-        auto plights_no_shadow_view = registry.view<const light::Point>(entt::exclude<components::ShadowCasting>);
-
-        plights_no_shadows_ssbo_.bind().update(
-            plights_no_shadow_view | ranges::views::transform([&](entt::entity e) {
-                return plights_with_shadow_view.get<const light::Point>(e);
-            })
-        );
-
-
-        // Draw the depth cubemaps for Point lights with the components::ShadowCasting.
-
-        sp_plight_depth_.use()
-            .and_then_with_self([&, this](ActiveShaderProgram& ashp) {
-
-            glViewport(0, 0, plight_shadow_maps.width(), plight_shadow_maps.height());
-
-            plight_shadow_maps.framebuffer().bind()
-                .and_then([&, this] {
-
-                    for (GLint layer{ 0 }; auto [_, plight]
-                        : plights_with_shadow_view.each())
-                    {
-                        if (layer == 0) /* first time */ {
-                            glClear(GL_DEPTH_BUFFER_BIT);
-                        }
-
-                        draw_scene_depth_cubemap(ashp, registry, plight.position, layer);
-
-                        // Layer as an index of a cubemap in the array, not as a 'layer-face'
-                        ++layer;
-                    }
-
-                })
-                .unbind();
-            });
-
-    }
+    void prepare_point_lights(const RenderEngine::PrimaryInterface&,
+        const entt::registry& registry);
 
 
     void draw_scene_depth_cubemap(ActiveShaderProgram& ashp,
-        const entt::registry& registry, const glm::vec3& position, gl::GLint cubemap_id)
-    {
-        glm::mat4 projection = glm::perspective(
-            glm::radians(90.f),
-            static_cast<float>(plight_shadow_maps.width()) /
-                static_cast<float>(plight_shadow_maps.height()),
-            plight_z_near_far.x, plight_z_near_far.y
-        );
-
-        ashp.uniform(locs_plight_.projection, projection);
-
-        const auto& basis = globals::basis;
-        views_mat_array_t views{
-            glm::lookAt(position, position + basis.x(), -basis.y()),
-            glm::lookAt(position, position - basis.x(), -basis.y()),
-            glm::lookAt(position, position + basis.y(), basis.z()),
-            glm::lookAt(position, position - basis.y(), -basis.z()),
-            glm::lookAt(position, position + basis.z(), -basis.y()),
-            glm::lookAt(position, position - basis.z(), -basis.y()),
-        };
+        const entt::registry& registry, const glm::vec3& position,
+        gl::GLint cubemap_id);
 
 
-        for (size_t i{ 0 }; i < views.size(); ++i) {
-            ashp.uniform(locs_plight_.views[i], views[i]);
-        }
-        ashp.uniform(locs_plight_.cubemap_id, cubemap_id);
-
-        ashp.uniform(locs_plight_.z_far, plight_z_near_far.y);
-
-        for (auto [_, transform, model]
-            : registry.view<const Transform, const Shared<Model>>().each())
-        {
-            ashp.uniform(locs_plight_.model, transform.mtransform().model());
-            for (auto& drawable : model->drawable_meshes()) {
-                drawable.mesh().draw();
-            }
-        }
-
-    }
-
-
-    glm::mat4 prepare_dir_light(const RenderEngine::PrimaryInterface& engine, const entt::registry& registry) {
-
-        using namespace gl;
-
-        glm::mat4 light_pv{};
-
-
-        for (auto [_, dir_light]
-            : registry.view<const light::Directional, components::ShadowCasting>().each())
-        {
-            glViewport(0, 0, dir_light_shadow_map.width(), dir_light_shadow_map.height());
-
-            glm::mat4 light_projection = glm::ortho(
-                -dir_light_projection_scale, dir_light_projection_scale,
-                -dir_light_projection_scale, dir_light_projection_scale,
-                dir_light_z_near_far.x, dir_light_z_near_far.y
-            );
-
-            glm::mat4 light_view = glm::lookAt(
-                engine.camera().get_pos() - dir_light_cam_offset * glm::normalize(dir_light.direction),
-                engine.camera().get_pos(),
-                globals::basis.y()
-            );
-
-            light_pv = light_projection * light_view;
-
-            sp_dir_depth_.use()
-                .and_then_with_self([&, this] (ActiveShaderProgram& ashp) {
-
-                    dir_light_shadow_map.framebuffer().bind()
-                        .and_then([&, this] {
-                            glClear(GL_DEPTH_BUFFER_BIT);
-
-                            ashp.uniform("projection", light_projection)
-                                .uniform("view", light_view);
-
-                            auto view = registry.view<Transform, Shared<Model>>();
-
-                            for (auto [_, transform, model] : view.each()) {
-
-                                ashp.uniform("model", transform.mtransform().model());
-
-                                for (auto& drawable : model->drawable_meshes()) {
-                                    drawable.mesh().draw();
-                                }
-                            }
-                        })
-                        .unbind();
-
-                });
-
-        }
-
-        return light_pv;
-    }
+    glm::mat4 prepare_dir_light(const RenderEngine::PrimaryInterface& engine,
+        const entt::registry& registry);
 
 
     void draw_scene(const RenderEngine::PrimaryInterface& engine,
-        const entt::registry& registry, const glm::mat4& dir_light_pv)
-    {
-        using namespace gl;
-
-        auto [w, h] = engine.window_size();
-        glViewport(0, 0, w, h);
-
-        sp_.use().and_then_with_self([&, this](ActiveShaderProgram& ashp) {
-
-            ashp.uniform(locs_.projection,
-                engine.camera().perspective_projection_mat(
-                    engine.window_size().aspect_ratio()
-                )
-            );
-            ashp.uniform(locs_.view, engine.camera().view_mat())
-                .uniform(locs_.cam_pos, engine.camera().get_pos());
-
-            // Ambient light.
-            for (auto [_, ambi] : registry.view<const light::Ambient>().each()) {
-                ashp.uniform(locs_.ambient_light.color, ambi.color);
-            }
-
-            // Dir light.
-            // FIXME: I stopped caring about Locations here...
-            for (auto [e, dir] : registry.view<const light::Directional>().each()) {
-                ashp.uniform("dir_light.color", dir.color)
-                    .uniform("dir_light.direction", dir.direction)
-                    .uniform("dir_light_cast_shadows", registry.all_of<components::ShadowCasting>(e));
-            }
-            ashp.uniform("dir_light_pv", dir_light_pv)
-                .uniform("dir_shadow_bias_bounds", dir_shadow_bias_bounds)
-                .uniform("dir_light_pcf_samples", dir_light_pcf_samples)
-                .uniform("dir_light_shadow_map", 2);
-            dir_light_shadow_map.depth_target().bind_to_unit(GL_TEXTURE2);
-
-            // Point light properties are sent through SSBOs.
-            // Send the depth cubemap array for point light shadow calculation.
-            ashp.uniform(locs_.point_light_shadow_maps, 3);
-            plight_shadow_maps.depth_taget().bind_to_unit(GL_TEXTURE3);
-
-            // Extra settings for point light shadows.
-            ashp.uniform(locs_.point_light_z_far, plight_z_near_far.y)
-                .uniform(locs_.point_shadow_bias_bounds, point_shadow_bias_bounds)
-                .uniform("point_light_pcf_samples", point_light_pcf_samples)
-                .uniform("point_light_pcf_offset", point_light_pcf_offset)
-                .uniform("point_light_use_fixed_pcf_samples", point_light_use_fixed_pcf_samples);
-
-            for (auto [_, transform, model]
-                : registry.view<const Transform, const Shared<Model>>().each())
-            {
-                auto model_transform = transform.mtransform();
-                ashp.uniform(locs_.model, model_transform.model())
-                    .uniform(locs_.normal_model, model_transform.normal_model());
-
-                model->draw(ashp, locs_.mat_ds);
-            }
-
-        });
-    }
-
+        const entt::registry& registry, const glm::mat4& dir_light_pv);
 
 
 };
@@ -402,6 +199,245 @@ private:
 
 
 
+inline void MaterialDSMultilightShadowStage::prepare_point_lights(
+    const RenderEngine::PrimaryInterface&,
+    const entt::registry& registry)
+{
+
+    using namespace gl;
+
+    // Update SSBOs for point lights.
+
+    auto plights_with_shadow_view = registry.view<const light::Point, const components::ShadowCasting>();
+
+    const size_t old_size = plights_with_shadows_ssbo_.size();
+
+    plights_with_shadows_ssbo_.bind().update(
+        plights_with_shadow_view | ranges::views::transform([&](entt::entity e) {
+            return plights_with_shadow_view.get<const light::Point>(e);
+        })
+    );
+
+    const bool num_plights_with_shadows_changed =
+        old_size != plights_with_shadows_ssbo_.size();
+
+    if (num_plights_with_shadows_changed) {
+        plight_shadow_maps.reset_size(
+            plight_shadow_maps.width(), plight_shadow_maps.height(),
+            static_cast<GLint>(plights_with_shadows_ssbo_.size())
+        );
+    }
+
+
+    auto plights_no_shadow_view = registry.view<const light::Point>(entt::exclude<components::ShadowCasting>);
+
+    plights_no_shadows_ssbo_.bind().update(
+        plights_no_shadow_view | ranges::views::transform([&](entt::entity e) {
+            return plights_with_shadow_view.get<const light::Point>(e);
+        })
+    );
+
+
+    // Draw the depth cubemaps for Point lights with the components::ShadowCasting.
+
+    sp_plight_depth_.use()
+        .and_then_with_self([&, this](ActiveShaderProgram& ashp) {
+
+        glViewport(0, 0, plight_shadow_maps.width(), plight_shadow_maps.height());
+
+        plight_shadow_maps.framebuffer().bind()
+            .and_then([&, this] {
+
+                for (GLint layer{ 0 }; auto [_, plight]
+                    : plights_with_shadow_view.each())
+                {
+                    if (layer == 0) /* first time */ {
+                        glClear(GL_DEPTH_BUFFER_BIT);
+                    }
+
+                    draw_scene_depth_cubemap(ashp, registry, plight.position, layer);
+
+                    // Layer as an index of a cubemap in the array, not as a 'layer-face'
+                    ++layer;
+                }
+
+            })
+            .unbind();
+        });
+
+}
+
+
+
+
+inline void MaterialDSMultilightShadowStage::draw_scene_depth_cubemap(
+    ActiveShaderProgram& ashp, const entt::registry& registry,
+    const glm::vec3& position, gl::GLint cubemap_id)
+{
+    glm::mat4 projection = glm::perspective(
+        glm::radians(90.f),
+        static_cast<float>(plight_shadow_maps.width()) /
+            static_cast<float>(plight_shadow_maps.height()),
+        plight_z_near_far.x, plight_z_near_far.y
+    );
+
+    ashp.uniform(locs_plight_.projection, projection);
+
+    const auto& basis = globals::basis;
+    views_mat_array_t views{
+        glm::lookAt(position, position + basis.x(), -basis.y()),
+        glm::lookAt(position, position - basis.x(), -basis.y()),
+        glm::lookAt(position, position + basis.y(), basis.z()),
+        glm::lookAt(position, position - basis.y(), -basis.z()),
+        glm::lookAt(position, position + basis.z(), -basis.y()),
+        glm::lookAt(position, position - basis.z(), -basis.y()),
+    };
+
+
+    for (size_t i{ 0 }; i < views.size(); ++i) {
+        ashp.uniform(locs_plight_.views[i], views[i]);
+    }
+    ashp.uniform(locs_plight_.cubemap_id, cubemap_id);
+
+    ashp.uniform(locs_plight_.z_far, plight_z_near_far.y);
+
+    for (auto [_, transform, model]
+        : registry.view<const Transform, const Shared<Model>>().each())
+    {
+        ashp.uniform(locs_plight_.model, transform.mtransform().model());
+        for (auto& drawable : model->drawable_meshes()) {
+            drawable.mesh().draw();
+        }
+    }
+
+}
+
+
+
+
+inline glm::mat4 MaterialDSMultilightShadowStage::prepare_dir_light(
+    const RenderEngine::PrimaryInterface& engine,
+    const entt::registry& registry)
+{
+
+    using namespace gl;
+
+    glm::mat4 light_pv{};
+
+
+    for (auto [_, dir_light]
+        : registry.view<const light::Directional, components::ShadowCasting>().each())
+    {
+        glViewport(0, 0, dir_light_shadow_map.width(), dir_light_shadow_map.height());
+
+        glm::mat4 light_projection = glm::ortho(
+            -dir_light_projection_scale, dir_light_projection_scale,
+            -dir_light_projection_scale, dir_light_projection_scale,
+            dir_light_z_near_far.x, dir_light_z_near_far.y
+        );
+
+        glm::mat4 light_view = glm::lookAt(
+            engine.camera().get_pos() - dir_light_cam_offset * glm::normalize(dir_light.direction),
+            engine.camera().get_pos(),
+            globals::basis.y()
+        );
+
+        light_pv = light_projection * light_view;
+
+        sp_dir_depth_.use()
+            .and_then_with_self([&, this] (ActiveShaderProgram& ashp) {
+
+                dir_light_shadow_map.framebuffer().bind()
+                    .and_then([&, this] {
+                        glClear(GL_DEPTH_BUFFER_BIT);
+
+                        ashp.uniform("projection", light_projection)
+                            .uniform("view", light_view);
+
+                        auto view = registry.view<Transform, Shared<Model>>();
+
+                        for (auto [_, transform, model] : view.each()) {
+
+                            ashp.uniform("model", transform.mtransform().model());
+
+                            for (auto& drawable : model->drawable_meshes()) {
+                                drawable.mesh().draw();
+                            }
+                        }
+                    })
+                    .unbind();
+
+            });
+
+    }
+
+    return light_pv;
+}
+
+
+
+
+inline void MaterialDSMultilightShadowStage::draw_scene(
+    const RenderEngine::PrimaryInterface& engine,
+    const entt::registry& registry, const glm::mat4& dir_light_pv)
+{
+    using namespace gl;
+
+    auto [w, h] = engine.window_size();
+    glViewport(0, 0, w, h);
+
+    sp_.use().and_then_with_self([&, this](ActiveShaderProgram& ashp) {
+
+        ashp.uniform(locs_.projection,
+            engine.camera().perspective_projection_mat(
+                engine.window_size().aspect_ratio()
+            )
+        );
+        ashp.uniform(locs_.view, engine.camera().view_mat())
+            .uniform(locs_.cam_pos, engine.camera().get_pos());
+
+        // Ambient light.
+        for (auto [_, ambi] : registry.view<const light::Ambient>().each()) {
+            ashp.uniform(locs_.ambient_light.color, ambi.color);
+        }
+
+        // Dir light.
+        // FIXME: I stopped caring about Locations here...
+        for (auto [e, dir] : registry.view<const light::Directional>().each()) {
+            ashp.uniform("dir_light.color", dir.color)
+                .uniform("dir_light.direction", dir.direction)
+                .uniform("dir_light_cast_shadows", registry.all_of<components::ShadowCasting>(e));
+        }
+        ashp.uniform("dir_light_pv", dir_light_pv)
+            .uniform("dir_shadow_bias_bounds", dir_shadow_bias_bounds)
+            .uniform("dir_light_pcf_samples", dir_light_pcf_samples)
+            .uniform("dir_light_shadow_map", 2);
+        dir_light_shadow_map.depth_target().bind_to_unit(GL_TEXTURE2);
+
+        // Point light properties are sent through SSBOs.
+        // Send the depth cubemap array for point light shadow calculation.
+        ashp.uniform(locs_.point_light_shadow_maps, 3);
+        plight_shadow_maps.depth_taget().bind_to_unit(GL_TEXTURE3);
+
+        // Extra settings for point light shadows.
+        ashp.uniform(locs_.point_light_z_far, plight_z_near_far.y)
+            .uniform(locs_.point_shadow_bias_bounds, point_shadow_bias_bounds)
+            .uniform("point_light_pcf_samples", point_light_pcf_samples)
+            .uniform("point_light_pcf_offset", point_light_pcf_offset)
+            .uniform("point_light_use_fixed_pcf_samples", point_light_use_fixed_pcf_samples);
+
+        for (auto [_, transform, model]
+            : registry.view<const Transform, const Shared<Model>>().each())
+        {
+            auto model_transform = transform.mtransform();
+            ashp.uniform(locs_.model, model_transform.model())
+                .uniform(locs_.normal_model, model_transform.normal_model());
+
+            model->draw(ashp, locs_.mat_ds);
+        }
+
+    });
+}
 
 
 
