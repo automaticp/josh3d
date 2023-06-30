ae0e178946703fb7fb81122d6406c2e55c186d54
automaticp <automaticpte@gmail.com>
2023-06-30 19:15:25 +0600

    extract QuadRenderer as a separate entity, endangering the existence of PostprocessRenderer

--- src/learnlib/render/PostprocessRenderer.hpp
+++ src/learnlib/render/PostprocessRenderer.hpp
@@ -1,32 +1,29 @@
 #pragma once
 #include "GLObjects.hpp"
-#include "Vertex2D.hpp"
+#include "QuadRenderer.hpp"
 
 
 namespace learn {
 
 
+/*
+Maybe this shouldn't exist?
+*/
 class PostprocessRenderer {
 private:
-    VBO quad_vbo_{};
-    VAO quad_vao_{};
+    QuadRenderer renderer_;
 
 public:
-    PostprocessRenderer() {
-        quad_vbo_.bind()
-            .attach_data(quad.size(), quad.data(), gl::GL_STATIC_DRAW)
-            .associate_with<Vertex2D>(quad_vao_.bind());
-    }
-
     void draw(ActiveShaderProgram& pp_shader, Texture2D& screen_color_texture) {
         using namespace gl;
+
         glDisable(GL_DEPTH_TEST);
 
         // These hardcoded uniform name and unit number are concerning...
         screen_color_texture.bind_to_unit(GL_TEXTURE0);
         pp_shader.uniform("color", 0);
 
-        quad_vao_.bind().draw_arrays(GL_TRIANGLES, 0, quad.size());
+        renderer_.draw();
     }
 
     // Emit a draw call of a simple quad covering the entire screen.
@@ -36,22 +33,8 @@ public:
         // FIXME: Whose job it is to disable depth testing?
         glDisable(GL_DEPTH_TEST);
 
-        quad_vao_.bind().draw_arrays(GL_TRIANGLES, 0, quad.size());
+        renderer_.draw();
     }
-
-
-private:
-    // Widning order must be counter-clockwise
-    // so that the faces would not be culled.
-    constexpr static std::array<Vertex2D, 6> quad{{
-        { { +1.0f, -1.0f }, { 1.0f, 0.0f } },
-        { { -1.0f, +1.0f }, { 0.0f, 1.0f } },
-        { { -1.0f, -1.0f }, { 0.0f, 0.0f } },
-
-        { { +1.0f, +1.0f }, { 1.0f, 1.0f } },
-        { { -1.0f, +1.0f }, { 0.0f, 1.0f } },
-        { { +1.0f, -1.0f }, { 1.0f, 0.0f } },
-    }};
 
 };
 
---
+++ src/learnlib/render/QuadRenderer.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include "GLBuffers.hpp"
+#include "Vertex2D.hpp"
+#include "GLScalars.hpp"
+#include <glbinding/gl/enum.h>
+
+
+namespace learn {
+
+
+/*
+Simple screen-filled quad render helper.
+
+Input attributes:
+
+    in vec2 pos;
+    in vec2 tex_coords;
+
+*/
+class QuadRenderer {
+private:
+    VBO quad_vbo_{};
+    VAO quad_vao_{};
+
+public:
+    QuadRenderer() {
+        using enum GLenum;
+
+        quad_vbo_.bind()
+            .attach_data(quad.size(), quad.data(), GL_STATIC_DRAW)
+            .associate_with<Vertex2D>(quad_vao_.bind());
+
+    }
+
+    // Emit a draw call on a quad.
+    // No other change of state, depth-testing is not disabled.
+    void draw() {
+        using enum GLenum;
+        quad_vao_.bind().draw_arrays(GL_TRIANGLES, 0, quad.size());
+    }
+
+private:
+    // Winding order is counter-clockwise
+    // so that the faces would not be culled.
+    constexpr static std::array<Vertex2D, 6> quad{{
+        { { +1.0f, -1.0f }, { 1.0f, 0.0f } },
+        { { -1.0f, +1.0f }, { 0.0f, 1.0f } },
+        { { -1.0f, -1.0f }, { 0.0f, 0.0f } },
+
+        { { +1.0f, +1.0f }, { 1.0f, 1.0f } },
+        { { -1.0f, +1.0f }, { 0.0f, 1.0f } },
+        { { +1.0f, -1.0f }, { 1.0f, 0.0f } },
+    }};
+
+};
+
+
+
+
+} // namespace learn
