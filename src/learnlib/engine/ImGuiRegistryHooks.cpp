61430e25c20eed2c5b5d811e774b3bd30b2ce607
automaticp <automaticpte@gmail.com>
2023-07-03 20:55:43 +0600

    update registry hooks to work with the new model/mesh layout

--- src/learnlib/engine/ImGuiRegistryHooks.cpp
+++ src/learnlib/engine/ImGuiRegistryHooks.cpp
@@ -1,10 +1,12 @@
 #include "ImGuiRegistryHooks.hpp"
 #include "AssimpModelLoader.hpp"
 #include "Model.hpp"
-#include "Shared.hpp"
+#include "MaterialDS.hpp"
 #include "Transform.hpp"
 #include "RenderComponents.hpp"
+#include "Vertex.hpp"
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/polar_coordinates.hpp>
 #include <imgui.h>
 
 
@@ -39,7 +41,22 @@ void ImGuiRegistryLightComponentsHook::operator()(entt::registry& registry) {
                     registry.remove<components::ShadowCasting>(e);
                 }
             }
-            ImGui::SliderFloat3("Direction", glm::value_ptr(dir.direction), -1.f, 1.f);
+
+
+            // TODO: Might actually make sense to represent direction
+            // as theta and phi pair internally. That way, there's no degeneracy.
+
+            // We swap x and y so that phi is rotation around x,
+            // and behaves more like the real Sun.
+            // We're probably not on the north pole, it's fine.
+            glm::vec3 swapped_dir{ dir.direction.y, dir.direction.x, dir.direction.z };
+            glm::vec2 polar = glm::degrees(glm::polar(swapped_dir));
+            if (ImGui::DragFloat2("Direction", glm::value_ptr(polar), 0.5f)) {
+                swapped_dir = glm::euclidean(glm::radians(glm::vec2{ polar.x, polar.y }));
+                // Un-swap back.
+                dir.direction = glm::vec3{ swapped_dir.y, swapped_dir.x, swapped_dir.z };
+            }
+
             ImGui::PopID();
         }
 
@@ -137,22 +154,72 @@ void ImGuiRegistryLightComponentsHook::operator()(entt::registry& registry) {
 
 
 
+
+
+
+
+static void display_transform_widget(Transform& transform) noexcept {
+
+    ImGui::DragFloat3(
+        "Position", glm::value_ptr(transform.position()),
+        0.2f, -100.f, 100.f
+    );
+
+    // FIXME: This is slightly more usable, but the singularity for Pitch around 90d
+    // is still unstable. In general: Local X is Pitch, Global Y is Yaw, and Local Z is Roll.
+    // Stll very messy to use, but should get the ball rolling.
+    const glm::quat& q = transform.rotation();
+    // Swap quaternion axes to make pitch around (local) X axis.
+    // Also GLM for some reason assumes that the locking [-90, 90] axis is
+    // associated with Yaw, not Pitch...? Maybe I am confused here but
+    // I want it Pitch, so we also have to swap the euler representation.
+    // (In my mind, Pitch and Yaw are Theta and Phi in spherical coordinates respectively).
+    const glm::quat q_shfl = glm::quat{ q.w, q.y, q.x, q.z };
+    glm::vec3 euler = glm::degrees(glm::vec3{
+        glm::yaw(q_shfl),   // Pitch
+        glm::pitch(q_shfl), // Yaw
+        glm::roll(q_shfl)   // Roll
+        // Dont believe what GLM says
+    });
+    if (ImGui::DragFloat3("Pitch/Yaw/Roll", glm::value_ptr(euler), 1.0f, -360.f, 360.f, "%.3f")) {
+        euler.x = glm::clamp(euler.x, -89.999f, 89.999f);
+        euler.y = glm::mod(euler.y, 360.f);
+        euler.z = glm::mod(euler.z, 360.f);
+        // Un-shuffle back both the euler angles and quaternions.
+        glm::quat p = glm::quat(glm::radians(glm::vec3{ euler.y, euler.x, euler.z }));
+        transform.rotation() = glm::quat{ p.w, p.y, p.x, p.z };
+    }
+
+    ImGui::DragFloat3(
+        "Scale", glm::value_ptr(transform.scaling()),
+        0.1f, 0.01f, 100.f, "%.3f", ImGuiSliderFlags_Logarithmic
+    );
+
+}
+
+
+
+
+
+
+
+
 void ImGuiRegistryModelComponentsHook::operator()(entt::registry& registry) {
 
-    if (ImGui::Button("Load")) {
+    if (ImGui::Button("Load (DS)")) {
+        entt::entity new_model;
         try {
-            Model model = AssimpModelLoader<>().load(load_path).get();
+            // Bit wasteful, but whatever for now
 
-            Shared<Model> model_ptr = std::make_shared<Model>(std::move(model));
+            new_model = registry.create();
+            ModelComponentLoader().load_into<Vertex, MaterialDS>(registry, new_model, load_path.c_str());
 
-            // Wasteful, but whatever for now
-
-            auto e = registry.create();
-            registry.emplace<Transform>(e);
-            registry.emplace<Shared<Model>>(e, std::move(model_ptr));
+            registry.emplace<Transform>(new_model);
+            registry.emplace<components::Path>(new_model, load_path);
 
             last_load_error_message = {};
         } catch (const error::AssimpLoaderError& e) {
+            registry.destroy(new_model);
             last_load_error_message = e.what();
         }
     }
@@ -163,77 +230,57 @@ void ImGuiRegistryModelComponentsHook::operator()(entt::registry& registry) {
 
     ImGui::Separator();
 
-    for (auto [e, transform, model] : registry.view<Transform, Shared<Model>>().each()) {
-        if (ImGui::TreeNode(reinterpret_cast<void*>(e), "Id %d", static_cast<entt::id_type>(e))) {
+    for (auto [e, transform, model] : registry.view<Transform, ModelComponent>().each()) {
+        const char* path = registry.all_of<components::Path>(e) ?
+            registry.get<components::Path>(e).path.c_str() : "(No Path)";
 
-            ImGui::DragFloat3(
-                "Position", glm::value_ptr(transform.position()),
-                0.2f, -100.f, 100.f
-            );
+        if (ImGui::TreeNode(reinterpret_cast<void*>(e), "Model [%d]: %s",
+            static_cast<entt::id_type>(e), path))
+        {
+
+            display_transform_widget(transform);
 
 
-            // ImGui::BeginDisabled();
-            // ImGui::DragFloat4("Quaternion", glm::value_ptr(transform.rotation()));
-            // ImGui::EndDisabled();
+            for (auto mesh_entity : model.meshes()) {
+                const char* name = registry.all_of<components::Name>(mesh_entity) ?
+                    registry.get<components::Name>(mesh_entity).name.c_str() : "(No Name)";
 
-            // FIXME: This is slightly more usable, but the singularity for Pitch around 90d
-            // is still unstable. In general: Local X is Pitch, Global Y is Yaw, and Local Z is Roll.
-            // Stll very messy to use, but should get the ball rolling.
-            const glm::quat& q = transform.rotation();
-            // Swap quaternion axes to make pitch around (local) X axis.
-            // Also GLM for some reason assumes that the locking [-90, 90] axis is
-            // associated with Yaw, not Pitch...? Maybe I am confused here but
-            // I want it Pitch, so we also have to swap the euler representation.
-            // (In my mind, Pitch and Yaw are Theta and Phi in spherical coordinates respectively).
-            const glm::quat q_shfl = glm::quat{ q.w, q.y, q.x, q.z };
-            glm::vec3 euler = glm::degrees(glm::vec3{
-                glm::yaw(q_shfl),   // Pitch
-                glm::pitch(q_shfl), // Yaw
-                glm::roll(q_shfl)   // Roll
-                // Dont believe what GLM says
-            });
-            if (ImGui::DragFloat3("Pitch/Yaw/Roll", glm::value_ptr(euler), 1.0f, -360.f, 360.f, "%.3f")) {
-                euler.x = glm::clamp(euler.x, -89.999f, 89.999f);
-                euler.y = glm::mod(euler.y, 360.f);
-                euler.z = glm::mod(euler.z, 360.f);
-                // Un-shuffle back both the euler angles and quaternions.
-                glm::quat p = glm::quat(glm::radians(glm::vec3{ euler.y, euler.x, euler.z }));
-                transform.rotation() = glm::quat{ p.w, p.y, p.x, p.z };
-            }
+                if (ImGui::TreeNode(reinterpret_cast<void*>(mesh_entity), "Mesh [%d]: %s",
+                    static_cast<entt::id_type>(mesh_entity), name))
+                {
 
-            ImGui::DragFloat3(
-                "Scale", glm::value_ptr(transform.scaling()),
-                0.1f, 0.01f, 100.f, "%.3f", ImGuiSliderFlags_Logarithmic
-            );
+                    display_transform_widget(registry.get<Transform>(mesh_entity));
 
-            if (ImGui::TreeNode("Materials")) {
-                for (size_t mat_id{ 0 }; auto& drawable : model->drawable_meshes()) {
-                    if (ImGui::TreeNode(reinterpret_cast<void*>(mat_id), "Material %zu", mat_id)) {
+                    MaterialDS* material = registry.try_get<MaterialDS>(mesh_entity);
 
-                        ImGui::Image(
-                            reinterpret_cast<ImTextureID>(drawable.material().diffuse->id()),
-                            { 256.f, 256.f }
-                        );
+                    if (material) {
+                        if (ImGui::TreeNode("Material (DS)")) {
 
-                        ImGui::Image(
-                            reinterpret_cast<ImTextureID>(drawable.material().specular->id()),
-                            { 256.f, 256.f }
-                        );
+                            ImGui::Image(
+                                reinterpret_cast<ImTextureID>(material->diffuse->id()),
+                                { 256.f, 256.f }
+                            );
 
-                        ImGui::DragFloat(
-                            "Shininess", &drawable.material().shininess,
-                            1.0f, 0.1f, 1.e4f, "%.3f", ImGuiSliderFlags_Logarithmic
-                        );
+                            ImGui::Image(
+                                reinterpret_cast<ImTextureID>(material->specular->id()),
+                                { 256.f, 256.f }
+                            );
 
-                        ImGui::TreePop();
+                            ImGui::DragFloat(
+                                "Shininess", &material->shininess,
+                                1.0f, 0.1f, 1.e4f, "%.3f", ImGuiSliderFlags_Logarithmic
+                            );
+
+                            ImGui::TreePop();
+                        }
+
                     }
 
-                    ++mat_id;
-
+                    ImGui::TreePop();
                 }
 
-                ImGui::TreePop();
             }
+
 
             ImGui::TreePop();
         }
--- src/learnlib/engine/RenderComponents.hpp
+++ src/learnlib/engine/RenderComponents.hpp
@@ -1,17 +1,24 @@
 #pragma once
 #include "GLObjects.hpp"
 #include "Shared.hpp"
+#include <string>
 
 
 
 
 namespace learn::components {
 
-// WIP
-// I'll slowly fill out this with wrappers, tags and aliases
-// to have a semi-standard list of components used in rendering
+/*
+WIP
 
-// Empty component used to enable shadows being cast from various light sources.
+I'll slowly fill out this with wrappers, tags and aliases
+to have a semi-standard list of components used in rendering
+*/
+
+
+/*
+Empty component used to enable shadows being cast from various light sources.
+*/
 struct ShadowCasting {};
 
 
@@ -19,6 +26,16 @@ struct Skybox {
     Shared<Cubemap> cubemap;
 };
 
+
+
+struct Name {
+    std::string name;
+};
+
+
+struct Path {
+    std::string path;
+};
 
 
 } // namespace learn::components
