4039542a442a3475b843606b2f6ae7be9cf24458
automaticp <automaticpte@gmail.com>
2023-07-04 19:34:55 +0600

    add deferred geometry stage for any material whose shaders fit the template

--- src/learnlib/engine/DeferredGeometryAnyMaterialStage.hpp
+++ src/learnlib/engine/DeferredGeometryAnyMaterialStage.hpp
@@ -1,20 +1,53 @@
-#include "DeferredGeometryStage.hpp"
-#include "GLShaders.hpp"
-#include "MaterialDS.hpp"
+#pragma once
+#include "MaterialConcept.hpp"
+#include "SharedStorage.hpp"
+#include "GBuffer.hpp"
 #include "RenderEngine.hpp"
 #include <entt/entt.hpp>
 
 
-using namespace gl;
-
 namespace learn {
 
 
-void DeferredGeometryStage::operator()(
+/*
+Not really "any", but as long as the shaders match the uniforms, all is fine.
+
+Uniforms:
+
+    uniform mat4 projection;
+    uniform mat4 view;
+    uniform mat4 model;
+    uniform mat3 normal_model;
+
+Plus whatever the MaterialT requires.
+*/
+template<material MaterialT>
+class DeferredGeometryAnyMaterialStage {
+private:
+    ShaderProgram sp_;
+
+    SharedStorageMutableView<GBuffer> gbuffer_;
+
+public:
+    DeferredGeometryAnyMaterialStage(
+        SharedStorageMutableView<GBuffer> gbuffer_view,
+        ShaderProgram sp)
+        : gbuffer_{ std::move(gbuffer_view) }
+        , sp_{ std::move(sp) }
+    {}
+
+    void operator()(const RenderEnginePrimaryInterface&, const entt::registry&);
+
+};
+
+
+
+
+template<material MaterialT>
+inline void DeferredGeometryAnyMaterialStage<MaterialT>::operator()(
     const RenderEnginePrimaryInterface& engine,
     const entt::registry& registry)
 {
-
     gbuffer_->framebuffer().bind_draw().and_then([&, this] {
 
         sp_.use().and_then([&, this](ActiveShaderProgram& ashp) {
@@ -27,7 +60,7 @@ void DeferredGeometryStage::operator()(
 
 
             for (auto [_, transform, mesh, material]
-                : registry.view<Transform, Mesh, MaterialDS>(entt::exclude<components::ChildMesh>).each())
+                : registry.view<Transform, Mesh, MaterialT>(entt::exclude<components::ChildMesh>).each())
             {
                 auto model_transform = transform.mtransform();
                 ashp.uniform("model", model_transform.model())
@@ -39,7 +72,7 @@ void DeferredGeometryStage::operator()(
 
 
             for (auto [_, transform, mesh, material, as_child]
-                : registry.view<Transform, Mesh, MaterialDS, components::ChildMesh>().each())
+                : registry.view<Transform, Mesh, MaterialT, components::ChildMesh>().each())
             {
                 auto model_transform =
                     registry.get<Transform>(as_child.parent).mtransform() *
@@ -57,6 +90,10 @@ void DeferredGeometryStage::operator()(
 
     });
 }
+
+
+
+
 
 
 
