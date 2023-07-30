2edd06ddbedba55d000191e2bcd6ded2453f9d90
automaticp <automaticpte@gmail.com>
2023-07-31 04:23:49 +0600

    add OnValueCondition imgui state helper; could be a bad idea

--- src/josh3d/imgui/ImGuiHelpers.hpp
+++ src/josh3d/imgui/ImGuiHelpers.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <concepts>
-#include <imgui.h>
 #include <type_traits>
+#include <functional>
+#include <utility>
+#include <imgui.h>
 
 
 namespace josh {
@@ -18,7 +20,132 @@ inline void* void_id(EnumT value) noexcept {
 }
 
 
+
+/*
+An RAII-enabled "if statement wrapper".
+
+Serves as a maybe-useful tool where a value change is a signalling condition
+for some kind of action. Here's an example using `on_value_change()`:
+
+    void imgui_widget(std::list<Thing>& list) {
+
+        if (ImGui::Begin("Things")) {
+            auto to_remove = on_value_change(
+                list.cend(),                 // Sentinel, if value changes to something else, then calls:
+                [&](const auto& remove_it) { // Called in the destructor of OnValueCodition.
+                    list.erase(remove_it);
+                }
+            );
+
+            for (auto it = list.begin(); it != list.end(); ++it) {
+                // ...
+
+                if (ImGui::Button("Remove Me")) {
+                    // Signal by changing the value to non-sentinel.
+                    to_remove.set(it);
+                }
+
+                // ...
+            }
+
+            // Will check if the value changed and do list.erase() if it did.
+        }
+        ImGui::End();
+
+    }
+
+Similar to SCOPE_EXIT and other scope guards, with a specific purpose.
+
+Useful if you want to insert/remove elements, but the container
+does not allow you to do that when iterating over it.
+*/
+template<typename ValueT, typename ConditionFunT, typename ResetFunT>
+    requires std::convertible_to<std::invoke_result_t<ConditionFunT, const ValueT&>, bool>
+class OnValueCondition {
+private:
+    ValueT value_;
+    ConditionFunT condition_;
+    ResetFunT reset_fun_;
+
+    template<typename ValT, typename ConditionF, typename ResetF>
+    friend auto on_value_condition(ValT value, ConditionF&& condition, ResetF&& reset_fun)
+        -> OnValueCondition<ValT, ConditionF, ResetF>;
+
+    OnValueCondition(ValueT value, ConditionFunT&& condition, ResetFunT&& reset_fun)
+        : value_{ std::move(value) }
+        , condition_{ std::forward<ConditionFunT>(condition) }
+        , reset_fun_{ std::forward<ResetFunT>(reset_fun) }
+    {}
+
+public:
+    void set(ValueT signal_value)
+        noexcept(noexcept(value_ = std::move(signal_value)))
+    {
+        value_ = std::move(signal_value);
+    }
+
+    OnValueCondition(const OnValueCondition&) = delete;
+    OnValueCondition(OnValueCondition&&) = delete;
+    OnValueCondition& operator=(const OnValueCondition&) = delete;
+    OnValueCondition& operator=(OnValueCondition&&) = delete;
+
+    ~OnValueCondition() noexcept(noexcept(reset())) { reset(); }
+
+private:
+    void reset()
+        noexcept(noexcept(std::invoke(condition_, value_)) && noexcept(std::invoke(reset_fun_, value_)))
+        requires std::invocable<ResetFunT, ValueT&>
+    {
+        if (std::invoke(std::forward<ConditionFunT>(condition_), std::as_const(value_))) {
+            std::invoke(std::forward<ResetFunT>(reset_fun_), value_);
+        }
+    }
+
+    void reset()
+        noexcept(noexcept(std::invoke(condition_, value_)) && noexcept(std::invoke(reset_fun_)))
+        requires std::invocable<ResetFunT>
+    {
+        if (std::invoke(std::forward<ConditionFunT>(condition_), std::as_const(value_))) {
+            std::invoke(std::forward<ResetFunT>(reset_fun_));
+        }
+    }
+
+};
+
+
+
+
+// Factory constructor that preserves value category
+// as opposed to CTAD of OnValueCondition.
+template<typename ValT, typename ConditionF, typename ActionF>
+[[nodiscard]] auto on_value_condition(ValT initial_value, ConditionF&& condition, ActionF&& reset_fun)
+    -> OnValueCondition<ValT, ConditionF, ActionF>
+{
+    return {
+        std::move(initial_value),
+        std::forward<ConditionF>(condition),
+        std::forward<ActionF>(reset_fun)
+    };
+}
+
+
+// OnValueCondition that triggers when the value changed
+// from initial sentinel.
+template<typename ValT, typename ActionF>
+[[nodiscard]] auto on_value_change_from(ValT sentinel_value, ActionF&& action_fun) {
+    return on_value_condition(
+        std::move(sentinel_value),
+        [=](const ValT& val) { return val != sentinel_value; },
+        std::forward<ActionF>(action_fun)
+    );
+}
+
+
+
+
 } // namespace josh
+
+
 
 
 namespace ImGui {
