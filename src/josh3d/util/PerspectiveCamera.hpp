fbb2c2ec16787172e6785ecadec5ecc923661c7a
automaticp <automaticpte@gmail.com>
2023-08-16 05:49:27 +0600

    move common camera functionality out of PerspectiveCamera into an implementation base class and use new representations for camera frustum

--- src/josh3d/util/PerspectiveCamera.hpp
+++ src/josh3d/util/PerspectiveCamera.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Basis.hpp"
 #include "GlobalsUtil.hpp"
+#include "CameraBase.hpp"
 #include "Transform.hpp"
 #include "ViewFrustum.hpp"
 #include <glm/ext/matrix_clip_space.hpp>
@@ -19,50 +20,28 @@ struct PerspectiveCameraParams {
 };
 
 
-class PerspectiveCamera {
+class PerspectiveCamera
+    : public detail::CameraBase<PerspectiveCamera>
+{
 private:
-    LocalViewFrustum frustum_;
     // We also store the same parameters that were used
     // to construct the frustum for future lookup.
     PerspectiveCameraParams params_;
+    using Base = detail::CameraBase<PerspectiveCamera>;
 
 public:
-    // The camera isn't an entity as of now, so we store
-    // the transform inside, available as a public member.
-    //
-    // The scale is so far ignored inside this class,
-    // but it will affect the constructible model matrix.
-    // If you DO set the scale to something other than (1, 1, 1),
-    // then there's a chance things become unexpectedly broken
-    // in other places. Good luck!
-    Transform transform;
-
-    PerspectiveCamera(PerspectiveCameraParams params) noexcept;
-    PerspectiveCamera(const Transform& transform, PerspectiveCameraParams params) noexcept;
-
-    // World-space ViewFrustum.
-    auto get_view_frustum() const noexcept
-        -> ViewFrustum;
-
-    // You probably want the world-space ViewFrustum instead.
-    auto get_local_view_frustum() const noexcept
-        -> const LocalViewFrustum&;
+    PerspectiveCamera(PerspectiveCameraParams params, const Transform& transform = {}) noexcept;
 
     auto get_params() const noexcept
         -> const PerspectiveCameraParams&;
 
     void update_params(const PerspectiveCameraParams& new_params) noexcept;
 
-    // Constructs local camera basis from the current transform:
-    // x - right, y - up, z - back.
-    auto get_local_basis() const noexcept
-        -> OrthonormalBasis3D;
-
-    auto view_mat() const noexcept
-        -> glm::mat4;
-
     auto projection_mat() const noexcept
         -> glm::mat4;
+
+private:
+    void update_frustum_representations() noexcept;
 
 };
 
@@ -70,45 +49,22 @@ public:
 
 
 inline PerspectiveCamera::PerspectiveCamera(
-    PerspectiveCameraParams params) noexcept
-    : PerspectiveCamera{ Transform{}, params }
-{}
-
-
-inline PerspectiveCamera::PerspectiveCamera(
-    const Transform& transform,
-    PerspectiveCameraParams params) noexcept
-    : frustum_{
-        LocalViewFrustum::from_perspective(
+    PerspectiveCameraParams params,
+    const Transform& transform) noexcept
+    : Base{
+        ViewFrustumAsPlanes::make_local_perspective(
             params.fovy_rad, params.aspect_ratio,
-            params.z_near, params.z_far)
+            params.z_near, params.z_far
+        ),
+        ViewFrustumAsQuads::make_local_perspective(
+            params.fovy_rad, params.aspect_ratio,
+            params.z_near, params.z_far
+        ),
+        transform
     }
     , params_{ params }
-    , transform{ transform }
 {}
 
-
-
-
-inline auto PerspectiveCamera::get_view_frustum() const noexcept
-    -> ViewFrustum
-{
-    return ViewFrustum::from_local_frustum(
-        frustum_,
-        Transform{
-            transform.position(),
-            transform.rotation(),
-            glm::vec3{ 1.f } // Strip away the scale.
-        }
-    );
-}
-
-
-inline auto PerspectiveCamera::get_local_view_frustum() const noexcept
-    -> const LocalViewFrustum&
-{
-    return frustum_;
-}
 
 
 inline auto PerspectiveCamera::get_params() const noexcept
@@ -122,31 +78,7 @@ inline void PerspectiveCamera::update_params(
     const PerspectiveCameraParams& params) noexcept
 {
     params_ = params;
-    frustum_ = LocalViewFrustum::from_perspective(
-        params_.fovy_rad, params_.aspect_ratio,
-        params_.z_near, params_.z_far
-    );
-}
-
-
-inline auto PerspectiveCamera::get_local_basis() const noexcept
-    -> OrthonormalBasis3D
-{
-    OrthonormalBasis3D basis{ globals::basis };
-    basis.rotate(transform.rotation());
-    return basis;
-}
-
-
-inline auto PerspectiveCamera::view_mat() const noexcept
-    -> glm::mat4
-{
-    auto local_basis = get_local_basis();
-    return glm::lookAt(
-        transform.position(),
-        transform.position() - local_basis.z(),
-        local_basis.y()
-    );
+    update_frustum_representations();
 }
 
 
@@ -158,6 +90,20 @@ inline auto PerspectiveCamera::projection_mat() const noexcept
         params_.z_near, params_.z_far
     );
 }
+
+
+inline void PerspectiveCamera::update_frustum_representations() noexcept {
+    planes_local_frustum_ = ViewFrustumAsPlanes::make_local_perspective(
+        params_.fovy_rad, params_.aspect_ratio,
+        params_.z_near, params_.z_far
+    );
+    quads_local_frustum_ = ViewFrustumAsQuads::make_local_perspective(
+        params_.fovy_rad, params_.aspect_ratio,
+        params_.z_near, params_.z_far
+    );
+}
+
+
 
 
 } // namespace josh
