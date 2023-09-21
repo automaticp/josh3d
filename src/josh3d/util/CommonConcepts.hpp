9218e6dc32c263c0334449124ce659e64074cfe6
automaticp <automaticpte@gmail.com>
2023-09-22 02:04:22 +0600

    add some `any_of`-like concepts

--- src/josh3d/util/CommonConcepts.hpp
+++ src/josh3d/util/CommonConcepts.hpp
@@ -12,6 +12,18 @@ concept same_as_remove_cvref =
     std::same_as<std::remove_cvref_t<T>, std::remove_cvref_t<U>>;
 
 
+template<typename T, typename ...Others>
+concept any_of = (std::same_as<T, Others> || ...);
+
+
+template<typename T, typename ...Others>
+concept any_of_remove_cvref = (same_as_remove_cvref<T, Others> || ...);
+
+
+template<typename T, typename ...Others>
+concept derived_from_any_of = (std::derived_from<T, Others> || ...);
+
+
 template<typename T, typename ...Args>
 concept not_move_or_copy_constructor_of =
     !(sizeof...(Args) == 1 && (same_as_remove_cvref<T, Args> && ...));
