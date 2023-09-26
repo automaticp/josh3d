473fa907d352aa25cc24494fc83cb235597e835f
automaticp <automaticpte@gmail.com>
2023-09-26 13:58:44 +0600

    add GLTexInfo reflection to Renderbuffers for use as attachments

--- src/josh3d/gl/GLRenderbuffer.hpp
+++ src/josh3d/gl/GLRenderbuffer.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include "detail/AndThen.hpp"
-#include "GLKindHandles.hpp"
-#include "Size.hpp"
+#include "detail/MagicConstructorsMacro.hpp"
 #include "GLScalars.hpp"
 #include "GLMutability.hpp" // IWYU pragma: keep (concepts)
-#include "detail/MagicConstructorsMacro.hpp"
+#include "GLKindHandles.hpp"
+#include "GLTextures.hpp"
+#include "Size.hpp"
+#include <glbinding/gl/enum.h>
 #include <glbinding/gl/gl.h>
 
 
@@ -15,10 +17,28 @@ template<mutability_tag MutT> class RawRenderbuffer;
 
 
 
+/*
+We treat renderbuffers similar to textures for reflection with GLTexInfo.
+This helps when using them as framebuffer attachments.
+*/
+template<> struct GLTexSpec<gl::GL_RENDERBUFFER> {
+    GLenum  internal_format;
+    GLsizei num_samples;
+    GLTexSpec(GLenum internal_format, GLsizei num_samples = 1)
+        : internal_format{ internal_format }, num_samples{ num_samples }
+    {}
+};
+
+namespace detail {
+template<> struct GLTexSizeImpl<gl::GL_RENDERBUFFER> { using type = Size2I; };
+} // namespace detail
+
+
 
 template<mutability_tag MutT>
 class BoundRenderbuffer
     : public detail::AndThen<BoundRenderbuffer<MutT>>
+    , public detail::GLTexInfo<gl::GL_RENDERBUFFER>
 {
 private:
     friend RawRenderbuffer<MutT>;
@@ -57,6 +77,7 @@ template<mutability_tag MutT>
 class RawRenderbuffer
     : public RawRenderbufferHandle<MutT>
     , public detail::ObjectHandleTypeInfo<RawRenderbuffer, MutT>
+    , public detail::GLTexInfo<gl::GL_RENDERBUFFER>
 {
 public:
     JOSH3D_MAGIC_CONSTRUCTORS(RawRenderbuffer, MutT, RawRenderbufferHandle<MutT>)
