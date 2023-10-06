2d4d7190b86b20d5153f7a5d77759a1fde53c687
automaticp <automaticpte@gmail.com>
2023-10-06 12:01:35 +0600

    add support for buffer mapping to GLBuffers

--- src/josh3d/gl/GLBuffers.hpp
+++ src/josh3d/gl/GLBuffers.hpp
@@ -1,14 +1,18 @@
 #pragma once
+#include "EnumUtils.hpp"
 #include "detail/AndThen.hpp"
 #include "detail/MagicConstructorsMacro.hpp"
 #include "GLMutability.hpp"
 #include "GLScalars.hpp"
 #include "GLKindHandles.hpp"
 #include "GLVertexArray.hpp"
+#include <glbinding/gl/bitfield.h>
 #include <glbinding/gl/enum.h>
 #include <glbinding/gl/functions.h>
 #include <glbinding/gl/gl.h>
 #include <glbinding/gl/types.h>
+#include <cassert>
+#include <cstddef>
 #include <span>
 
 
@@ -60,9 +64,14 @@ template<
     template<typename> typename BoundIndexedT,
     GLenum TargetV
 >
-struct BindableBufferIndexed
-    : BindableBuffer<MutT, CRTP, BoundT, TargetV>
-{
+struct BindableBufferIndexed {
+    BoundT<MutT> bind() const noexcept {
+        gl::glBindBuffer(
+            TargetV, static_cast<const CRTP<MutT>&>(*this).id()
+        );
+        return {};
+    }
+
     BoundIndexedT<MutT> bind_to_index(GLuint binding_index) const noexcept {
         gl::glBindBufferBase(
             TargetV, binding_index,
@@ -121,6 +130,27 @@ struct BoundBufferBase {
 
 
 
+template<typename T>
+std::span<T> map_buffer_range_impl(
+    GLenum target, GLintptr elem_offset, GLsizeiptr elem_count,
+    gl::MapBufferAccessMask access, gl::MapBufferAccessMask rw_bits)
+{
+    constexpr auto rw_maskout =
+        gl::MapBufferAccessMask{
+            ~to_underlying( // Bitfields in glbinding don't support `operator~`...
+                gl::MapBufferAccessMask{ gl::GL_MAP_READ_BIT | gl::GL_MAP_WRITE_BIT }
+            )
+        };
+    access = (access & rw_maskout) | rw_bits;
+    void* buf =
+        gl::glMapBufferRange(
+            target, elem_offset * sizeof(T), elem_count * sizeof(T), access
+        );
+    return { static_cast<T*>(buf), elem_count };
+}
+
+
+
 
 template<typename CRTP, GLenum TargetV>
 struct BoundBufferConstImpl
@@ -131,6 +161,52 @@ struct BoundBufferConstImpl
         gl::glGetBufferSubData(
             TargetV, offset * sizeof(T), dst_buf.size_bytes(), dst_buf.data()
         );
+        return static_cast<CRTP&>(*this);
+    }
+
+    GLsizeiptr get_size_bytes() {
+        GLint64 size;
+        gl::glGetBufferParameteri64v(TargetV, gl::GL_BUFFER_SIZE, &size);
+        return size;
+    }
+
+    template<typename T>
+    GLsizeiptr get_size() {
+        return get_size_bytes() / sizeof(T);
+    }
+
+    // Maps a subrange of the buffer storage for read-only operations.
+    // The Read/Write bits in `access` are ignored by this call.
+    template<typename T>
+    [[nodiscard]] std::span<const T> map_for_read(
+        GLintptr elem_offset, GLsizeiptr elem_count, gl::MapBufferAccessMask access = {})
+    {
+        return map_buffer_range_impl<T>(
+            TargetV, elem_offset, elem_count,
+            access, gl::GL_MAP_READ_BIT
+        );
+    }
+
+    // Maps a the entire buffer storage for read-only operations.
+    // The Read/Write bits in `access` are ignored by this call.
+    template<typename T>
+    [[nodiscard]] std::span<const T> map_for_read(gl::MapBufferAccessMask access = {}) {
+        return map_for_read<T>(0, get_size<T>(), access);
+    }
+
+    CRTP& unmap_current() {
+        // This can fail according to spec, but does it ever?
+        //
+        // Quote:
+        //     "glUnmapBuffer returns GL_TRUE unless the data store contents
+        //     have become corrupt during the time the data store was mapped.
+        //     This can occur for system-specific reasons that affect
+        //     the availability of graphics memory, such as screen mode changes.
+        //     In such situations, GL_FALSE is returned and the data store contents
+        //     are undefined. An application must detect this rare condition
+        //     and reinitialize the data store."
+        GLboolean unmapping_succeded = gl::glUnmapBuffer(TargetV);
+        assert(unmapping_succeded && "It's your unlucky day!");
         return static_cast<CRTP&>(*this);
     }
 };
@@ -161,6 +237,54 @@ struct BoundBufferMutableImpl
     CRTP& sub_data(std::span<const T> src_buf, GLsizeiptr offset = 0) {
         gl::glBufferSubData(
             TargetV, offset * sizeof(T), src_buf.size_bytes(), src_buf.data()
+        );
+        return static_cast<CRTP&>(*this);
+    }
+
+    // Maps a subrange of the buffer storage for write-only operations.
+    // The Read/Write bits in `access` are ignored by this call.
+    template<typename T>
+    [[nodiscard]] std::span<T> map_for_write(
+        GLintptr elem_offset, GLsizeiptr elem_count, gl::MapBufferAccessMask access = {})
+    {
+        return map_buffer_range_impl<T>(
+            TargetV, elem_offset, elem_count,
+            access, gl::GL_MAP_WRITE_BIT
+        );
+    }
+
+    // Maps the whole buffer storage for write-only operations.
+    // The Read/Write bits in `access` are ignored by this call.
+    template<typename T>
+    [[nodiscard]] std::span<T> map_for_write(gl::MapBufferAccessMask access = {}) {
+        return map_for_write<T>(0, this->template get_size<T>(), access);
+    }
+
+    // Maps a subrange of the buffer storage for read and write operations.
+    // The Read/Write bits in `access` are ignored by this call.
+    template<typename T>
+    [[nodiscard]] std::span<T> map_for_readwrite(
+        GLintptr elem_offset, GLsizeiptr elem_count, gl::MapBufferAccessMask access = {})
+    {
+        return map_buffer_range_impl<T>(
+            TargetV, elem_offset, elem_count,
+            access, (gl::GL_MAP_READ_BIT | gl::GL_MAP_WRITE_BIT)
+        );
+    }
+
+    // Maps the whole buffer storage for read and write operations.
+    // The Read/Write bits in `access` are ignored by this call.
+    template<typename T>
+    [[nodiscard]] std::span<T> map_for_readwrite(gl::MapBufferAccessMask access = {}) {
+        return map_for_readwrite<T>(0, this->template get_size<T>(), access);
+    }
+
+
+    // The buffer object must previously have been mapped with the `GL_MAP_FLUSH_EXPLICIT_BIT` flag.
+    template<typename T>
+    CRTP& flush_mapped_range(GLintptr elem_offset, GLsizeiptr elem_count) {
+        gl::glFlushMappedBufferRange(
+            TargetV, elem_offset * sizeof(T), elem_count * sizeof(T)
         );
         return static_cast<CRTP&>(*this);
     }
