d2587677f79be83061a71a2fa29716ef4b8e2ae3
automaticp <automaticpte@gmail.com>
2023-10-26 21:02:28 +0600

    remove old dot files before viz generation

--- docs/viz/gen_dot.sh
+++ docs/viz/gen_dot.sh
@@ -7,6 +7,8 @@ src_dir="../../"
 build_dir="$src_dir/build/"
 out_dir="./out-dot/"
 
+rm -v "$out_dir"/josh3d.dot*
+
 cp "CMakeGraphVizOptions.cmake" "$build_dir"
 
 cmake -S "$src_dir" -B "$build_dir" --graphviz="$out_dir/josh3d.dot"
