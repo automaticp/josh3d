6fcf0ecf29b7f549ec3e25f023cfd69a2c67a0dd
automaticp <automaticpte@gmail.com>
2023-11-11 15:14:12 +0600

    minor rework of the skybox loading interface

--- src/josh3d/imgui/hooks/SkyboxRegistryHook.cpp
+++ src/josh3d/imgui/hooks/SkyboxRegistryHook.cpp
@@ -2,6 +2,7 @@
 #include "CubemapData.hpp"
 #include "Filesystem.hpp"
 #include "GLTextures.hpp"
+#include "ImGuiHelpers.hpp"
 #include "Pixels.hpp"
 #include "TextureHelpers.hpp"
 #include "components/Skybox.hpp"
@@ -21,50 +22,57 @@ namespace josh::imguihooks {
 
 void SkyboxRegistryHook::operator()(entt::registry& registry) {
 
-    if (ImGui::TreeNode("Load Skybox")) {
-        ImGui::InputText("Cubemap JSON", &load_path_);
+    auto try_load_skybox = [&] {
+        try {
+            error_text_ = "";
 
-        if (ImGui::Button("Load")) {
-            try {
-                Path path{ load_path_ };
-                File skybox_json = std::invoke([&]() -> File {
-                    if (path.is_relative()) {
-                        return VPath(path);
-                    } else {
-                        return File(path);
-                    }
-                });
-                auto data = load_cubemap_from_json<pixel::RGBA>(skybox_json);
-
-                auto skybox_e = registry.view<components::Skybox>().back();
-                if (skybox_e == entt::null) {
-                    skybox_e = registry.create();
+            Path path{ load_path_ };
+            // TODO: emplace Path and VPath (optionally) like for models.
+            File skybox_json = std::invoke([&]() -> File {
+                if (path.is_relative()) {
+                    return VPath(path);
+                } else {
+                    return File(path);
                 }
-                auto& skybox =
-                    registry.emplace_or_replace<components::Skybox>(skybox_e, std::make_shared<UniqueCubemap>());
+            });
+            auto data = load_cubemap_from_json<pixel::RGBA>(skybox_json);
 
-                using enum GLenum;
-                skybox.cubemap->bind()
-                    .and_then([&](BoundCubemap<GLMutable>& cubemap) {
-                        attach_data_to_cubemap_as_skybox(
-                            cubemap, data, GL_SRGB_ALPHA
-                        );
-                    })
-                    .unbind();
-
-                error_text_ = "";
-            } catch (const std::runtime_error& e) {
-                error_text_ = e.what();
+            auto skybox_e = registry.view<components::Skybox>().back();
+            if (skybox_e == entt::null) {
+                skybox_e = registry.create();
             }
+            auto& skybox =
+                registry.emplace_or_replace<components::Skybox>(skybox_e, std::make_shared<UniqueCubemap>());
+
+            using enum GLenum;
+            skybox.cubemap->bind()
+                .and_then([&](BoundCubemap<GLMutable>& cubemap) {
+                    attach_data_to_cubemap_as_skybox(
+                        cubemap, data, GL_SRGB_ALPHA
+                    );
+                })
+                .unbind();
+
+        } catch (const std::runtime_error& e) {
+            error_text_ = e.what();
         }
+    };
 
-        ImGui::TextUnformatted(error_text_.c_str());
+    auto load_skybox_signal = on_value_change_from(false, try_load_skybox);
 
-        ImGui::TreePop();
+    if (ImGui::InputText("##Path or VPath", &load_path_,
+        ImGuiInputTextFlags_EnterReturnsTrue))
+    {
+        load_skybox_signal.set(true);
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Load from JSON")) {
+        load_skybox_signal.set(true);
     }
 
-}
+    ImGui::TextColored({ 1.0f, 0.5f, 0.5f, 1.0f }, "%s", error_text_.c_str());
 
+}
 
 
 } // namespace josh::imguihooks
