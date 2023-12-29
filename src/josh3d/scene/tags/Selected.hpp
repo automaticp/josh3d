ac31b368b45c2424fb1dcfc4b5fb0b518c09a5cb
automaticp <automaticpte@gmail.com>
2023-12-29 15:47:11 +0200

    add Selected tag

---
+++ src/josh3d/scene/tags/Selected.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+namespace josh::tags {
+
+
+/*
+Tag type denoting objects that are currently selected either via mouse-picking or otherwise.
+*/
+struct Selected {};
+
+
+} // namespace josh::tags
