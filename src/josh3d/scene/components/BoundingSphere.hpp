5e786311f95db5649c453813d2b21d047ed22caa
automaticp <automaticpte@gmail.com>
2024-04-30 18:54:16 +0300

    make bounding sphere a normal type first

--- src/josh3d/scene/components/BoundingSphere.hpp
+++ src/josh3d/scene/components/BoundingSphere.hpp
@@ -1,20 +1,11 @@
 #pragma once
-#include <glm/glm.hpp>
+#include <BoundingSphere.hpp>
 
 
 namespace josh::components {
 
 
-/*
-Simple pivot-centered sphere that fully encloses the object.
-*/
-struct BoundingSphere {
-    float radius;
-
-    float scaled_radius(const glm::vec3& scale) const noexcept {
-        return glm::max(glm::max(scale.x, scale.y), scale.z) * radius;
-    }
-};
+using josh::BoundingSphere;
 
 
 } // namespace josh::components
