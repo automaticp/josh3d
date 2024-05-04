61c1446cb670741d0397cee2759b9497a49dd261
automaticp <automaticpte@gmail.com>
2024-05-04 12:29:26 +0300

    add more tag helpers

--- src/josh3d/scene/ECSHelpers.hpp
+++ src/josh3d/scene/ECSHelpers.hpp
@@ -2,6 +2,7 @@
 #include "Transform.hpp"
 #include "components/ChildMesh.hpp"
 #include "components/Model.hpp"
+#include <entt/entity/fwd.hpp>
 #include <entt/entt.hpp>
 
 
@@ -61,16 +62,41 @@ inline size_t calculate_view_size(auto entt_view) noexcept {
 }
 
 
+// Just for clarity.
+template<typename TagT>
+inline bool has_tag(entt::handle handle) noexcept {
+    return handle.all_of<TagT>();
+}
+
+
+// Returns true if the tag was set succesfully, false if it was already set.
+template<typename TagT>
+inline bool set_tag(entt::handle handle) noexcept {
+    if (has_tag<TagT>(handle)) {
+        return false;
+    } else {
+        handle.emplace<TagT>();
+        return true;
+    }
+}
+
+
+template<typename TagT>
+inline bool unset_tag(entt::handle handle) noexcept {
+    return handle.remove<TagT>();
+}
+
+
 // Removes a tag from the entity if it has one,
 // adds a tag to the entity if it doesn't.
 // Returns true if the tag was added, false if it was removed.
 template<typename TagT>
 inline bool switch_tag(entt::handle handle) noexcept {
-    if (handle.any_of<TagT>()) {
-        handle.remove<TagT>();
+    if (has_tag<TagT>(handle)) {
+        unset_tag<TagT>(handle);
         return false;
     } else {
-        handle.emplace<TagT>();
+        set_tag<TagT>(handle);
         return true;
     }
 }
